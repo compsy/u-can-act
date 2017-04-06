ce7c46e977d94217909da2a39ac7a02795029e4f
Ando Emerencia <ando.emerencia@gmail.com>
2017-04-06 15:20:00 +0200

    added a single test

---
+++ spec/features/static_page_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'index page' do
+  it 'should load the index page' do
+    #visit '/'
+    #expect(page).to have_content('Yay! You’re on Rails!')
+    #expect(page.status_code).to eq(200)
+    expect(true).to be_truthy
+  end
+end

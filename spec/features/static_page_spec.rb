21a0a47a672c91729f549b262c283c595cba9b5b
Ando Emerencia <ando.emerencia@gmail.com>
2017-04-06 15:21:48 +0200

    updated gems and rubocop

--- spec/features/static_page_spec.rb
+++ spec/features/static_page_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 
 describe 'index page' do
   it 'should load the index page' do
-    #visit '/'
-    #expect(page).to have_content('Yay! You’re on Rails!')
-    #expect(page.status_code).to eq(200)
+    # visit '/'
+    # expect(page).to have_content('Yay!')
+    # expect(page.status_code).to eq(200)
     expect(true).to be_truthy
   end
 end

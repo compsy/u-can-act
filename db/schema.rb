7d30cfe5e4c6c9dfa2d468a76340b2f4139438db
Ando Emerencia <ando.emerencia@gmail.com>
2017-05-08 15:58:28 +0200

    add first and last name to person

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20170420142715) do
   create_table "people", force: :cascade do |t|
     t.string   "type",         null: false
     t.string   "mobile_phone", null: false
+    t.string   "first_name",   null: false
+    t.string   "last_name"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["mobile_phone"], name: "index_people_on_mobile_phone", unique: true, using: :btree

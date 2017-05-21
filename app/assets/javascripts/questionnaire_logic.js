b223d7a57040af5cd299fae16437dce3de64d709
Ando Emerencia <ando.emerencia@gmail.com>
2017-05-21 03:18:38 +0200

    allow clicking input textfield to toggle checkbox/radio state

--- app/assets/javascripts/questionnaire_logic.js
+++ app/assets/javascripts/questionnaire_logic.js
@@ -1,12 +1,17 @@
 function toggle_otherwise_field() {
-  if ($(this).closest('.row').find('.otherwise-option').is(':checked')) {
-    $(this).closest('.row').find("input[type=text].otherwise").prop('disabled', false);
+  if ($(this).closest('.otherwise-textfield').find('.otherwise-option').is(':checked')) {
+    $(this).closest('.otherwise-textfield').find("input[type=text].otherwise").prop('disabled', false).focus();
   } else {
-    $(this).closest('.row').find("input[type=text].otherwise").prop('disabled', true);
+    $(this).closest('.otherwise-textfield').find("input[type=text].otherwise").prop('disabled', true);
   }
 }
 
 $(function () {
   $('input[type=radio],input[type=checkbox]').change(toggle_otherwise_field);
   $('.otherwise-option').each(toggle_otherwise_field);
+  $('label + div.input-field.inline').click(function() {
+    if ($(this).find('input[type=text][disabled]').length) {
+      $(this).prev().click();
+    }
+  });
 });

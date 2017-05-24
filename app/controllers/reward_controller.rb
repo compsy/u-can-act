8a6490286437e752def4f8ff4633f813f270caf1
Ando Emerencia <ando.emerencia@gmail.com>
2017-05-24 14:08:08 +0200

    rubocop

--- app/controllers/reward_controller.rb
+++ app/controllers/reward_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class RewardController < ApplicationController
+  before_action :set_response_if_dev
   before_action :verify_response_id
   before_action :set_protocol_subscription
 
@@ -6,9 +9,13 @@ class RewardController < ApplicationController
 
   private
 
+  def set_response_if_dev
+    cookies.signed[:response_id] = Person.first.protocol_subscriptions.first.responses.first.id if
+      Rails.env.development?
+  end
+
   def verify_response_id
     # TODO: REMOVE ME
-    cookies.signed[:response_id] = Person.first.protocol_subscriptions.first.responses.first.id if Rails.env.development?
     return if cookies.signed[:response_id] && Response.find_by_id(cookies.signed[:response_id])
     render(status: 401, plain: 'Je kan deze pagina pas bekijken als je een vragenlijst hebt ingevuld.')
   end

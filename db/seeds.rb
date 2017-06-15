527cd0a71a34430959e06b41b4c29e2aced1583e
Ando Emerencia <ando.emerencia@gmail.com>
2017-06-15 15:42:26 +0200

    fixed collisions in seeds and created seeds for student nametingen

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@ ActiveRecord::Base.connection.reconnect! if Rails.env.development?
 # Require personal question seeds separately because they
 # need to already exist when the protocol seeds are loaded, and the
 # order is different on production servers.
-require File.join(File.dirname(__FILE__),'seeds','questionnaire_seeds.rb')
+require File.join(File.dirname(__FILE__), 'seeds', 'questionnaire_seeds.rb')
 # Load seeds from the seeds directory.
-Dir[File.join(File.dirname(__FILE__),'seeds','*.rb')].each do |file|
+Dir[File.join(File.dirname(__FILE__), 'seeds', '*.rb')].each do |file|
   require file
 end
 
@@ -48,23 +48,64 @@ if Rails.env.development?
     invited_state: Response::SENT_STATE)
   responseobj.initialize_invitation_token!
   puts "mentor posttest: #{Rails.application.routes.url_helpers.root_url}?q=#{responseobj.invitation_token.token}"
-  Student.first.protocol_subscriptions.create(
+
+  student = Student.first
+  student.protocol_subscriptions.create(
     protocol: Protocol.find_by_name('pilot - studenten 1x per week'),
     state: ProtocolSubscription::ACTIVE_STATE,
     start_date: Time.zone.now.beginning_of_week
   )
-  responseobj = Student.first.protocol_subscriptions.first.responses.first
+  responseobj = student.protocol_subscriptions.first.responses.first
   responseobj.update_attributes!(
     open_from: 1.minute.ago,
     invited_state: Response::SENT_STATE)
   responseobj.initialize_invitation_token!
   puts "student 1x per week questionnaire: #{Rails.application.routes.url_helpers.root_url}?q=#{responseobj.invitation_token.token}"
-  responseobj = Student.first.protocol_subscriptions.first.responses.last
+  responseobj = student.protocol_subscriptions.first.responses.last
   responseobj.update_attributes!(
     open_from: 1.minute.ago,
     invited_state: Response::SENT_STATE)
   responseobj.initialize_invitation_token!
   puts "student 1x per week posttest: #{Rails.application.routes.url_helpers.root_url}?q=#{responseobj.invitation_token.token}"
+
+  student = Student.second
+  student.protocol_subscriptions.create(
+    protocol: Protocol.find_by_name('pilot - studenten 2x per week'),
+    state: ProtocolSubscription::ACTIVE_STATE,
+    start_date: Time.zone.now.beginning_of_week
+  )
+  responseobj = student.protocol_subscriptions.first.responses.first
+  responseobj.update_attributes!(
+    open_from: 1.minute.ago,
+    invited_state: Response::SENT_STATE)
+  responseobj.initialize_invitation_token!
+  puts "student 2x per week questionnaire: #{Rails.application.routes.url_helpers.root_url}?q=#{responseobj.invitation_token.token}"
+  responseobj = student.protocol_subscriptions.first.responses.last
+  responseobj.update_attributes!(
+    open_from: 1.minute.ago,
+    invited_state: Response::SENT_STATE)
+  responseobj.initialize_invitation_token!
+  puts "student 2x per week posttest: #{Rails.application.routes.url_helpers.root_url}?q=#{responseobj.invitation_token.token}"
+
+  student = Student.third
+  student.protocol_subscriptions.create(
+    protocol: Protocol.find_by_name('pilot - studenten 5x per week'),
+    state: ProtocolSubscription::ACTIVE_STATE,
+    start_date: Time.zone.now.beginning_of_week
+  )
+  responseobj = student.protocol_subscriptions.first.responses.first
+  responseobj.update_attributes!(
+    open_from: 1.minute.ago,
+    invited_state: Response::SENT_STATE)
+  responseobj.initialize_invitation_token!
+  puts "student 5x per week questionnaire: #{Rails.application.routes.url_helpers.root_url}?q=#{responseobj.invitation_token.token}"
+  responseobj = student.protocol_subscriptions.first.responses.last
+  responseobj.update_attributes!(
+    open_from: 1.minute.ago,
+    invited_state: Response::SENT_STATE)
+  responseobj.initialize_invitation_token!
+  puts "student 5x per week posttest: #{Rails.application.routes.url_helpers.root_url}?q=#{responseobj.invitation_token.token}"
+
   puts ""
 end
 

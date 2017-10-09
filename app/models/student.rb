2369ebdbdb4d9d96665251d10034ff283682ac3f
Ando Emerencia <ando.emerencia@gmail.com>
2017-10-09 18:31:01 +0200

    add a warning when multiple mentors are found for a student

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,14 @@
 
 class Student < Person
   def mentor
+    warn_for_multiple_mentors
     ProtocolSubscription.where(filling_out_for_id: id).where.not(person_id: id).first&.person
+  end
+
+  private
+
+  def warn_for_multiple_mentors
+    Rails.logger.warn "[Attention] retrieving one of multiple mentors for student: #{student.id}" if
+      ProtocolSubscription.where(filling_out_for_id: id).where.not(person_id: id).count > 1
   end
 end

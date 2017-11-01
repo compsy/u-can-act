c30a48e0e8ba8441c34a1ccea7f4ea39754dd919
Ando Emerencia <ando.emerencia@gmail.com>
2017-11-01 15:40:22 +0100

    removed debugging

--- app/generators/questionnaire_generator.rb
+++ app/generators/questionnaire_generator.rb
@@ -343,7 +343,6 @@ class QuestionnaireGenerator
     end
 
     def generate_textarea(question)
-      Rails.logger.info question
       title = safe_join([question[:title].html_safe, generate_tooltip(question[:tooltip])])
       safe_join([content_tag(:p, title, class: 'flow-text'), textarea_field(question)])
     end

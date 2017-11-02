299237eb1c3cd1b8a393fc82ab835c4a5d3c0168
Ando Emerencia <ando.emerencia@gmail.com>
2017-11-02 15:47:01 +0100

    fixed outlining and section end

--- db/seeds/questionnaires/mentor_diary.rb
+++ db/seeds/questionnaires/mentor_diary.rb
@@ -128,7 +128,8 @@ dagboek_content = [{
   hidden: true,
   type: :range,
   title: 'In hoeverre heb jij deze week geprobeerd {{deze_student}} het gevoel te geven dat {{hij_zij_student}} dingen goed kan?',
-  labels: ['niet', 'heel sterk']
+  labels: ['niet', 'heel sterk'],
+  section_end: true
 }, {
   id: :v8, # 3.2
   hidden: true,

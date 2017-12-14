f4d70b3af988b5ef5e4c5eebab9bfe8697afe155
Frank Blaauw <frank.blaauw@gmail.com>
2017-12-14 10:57:31 +0100

    Minor changes, added manual inclue of heatmap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,8 +19,8 @@
 //= require react_ujs
 //= require highcharts
 //= require highcharts/highcharts-more
-//= require highcharts/modules/heatmap
 //= require highcharts/modules/exporting
+//  require highcharts/modules/heatmap
 //= require highcharts/modules/data
 //= require highcharts/modules/boost-canvas
 //= require highcharts/modules/boost

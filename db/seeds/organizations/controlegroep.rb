509bd4979f1cc9d6482438e52c8c4f0b7cd78927
Frank Blaauw <frank.blaauw@gmail.com>
2018-01-17 15:05:35 +0100

    Remove useless lines

--- db/seeds/organizations/controlegroep.rb
+++ db/seeds/organizations/controlegroep.rb
@@ -1,5 +1,4 @@
 organization_name = 'Controlegroep'
-titles = []
 
 puts "Running seeds for #{organization_name}"
 organization = Organization.find_by_name(organization_name)
@@ -7,8 +6,3 @@ organization ||= Organization.create!(name: organization_name)
 
 student = organization.roles.where(group: Person::STUDENT).first
 student ||= organization.roles.create!(group: Person::STUDENT, title: Person::STUDENT)
-
-titles.each do |title|
-  entry = organization.roles.where(group: Person::MENTOR, title: title).first
-  entry ||= organization.roles.create!(group: Person::MENTOR, title: title)
-end

32e2069f79513294bf30fc1632cc59691e78c595
Ando Emerencia <ando.emerencia@gmail.com>
2018-03-05 17:52:11 +0100

    fixed invitationtoken cleanup spec

--- spec/background_tasks/cleanup_invitation_tokens_spec.rb
+++ spec/background_tasks/cleanup_invitation_tokens_spec.rb
@@ -12,22 +12,43 @@ describe CleanupInvitationTokens do
     describe 'loops through all invitation tokens' do
       # This is also tested without mocking the expired? function in a feature test.
       it 'should destroy invitation tokens if they are expired' do
-        FactoryBot.create_list(:invitation_token, 17)
-        allow_any_instance_of(InvitationToken).to receive(:expired?).and_return(true)
+        FactoryBot.create_list(:invitation_token, 17, created_at: 8.days.ago)
         expect(InvitationToken.count).to eq 17
         described_class.run
         expect(InvitationToken.count).to be_zero
-        expect(Response.count).to eq 17
       end
 
       it 'should not destroy invitation tokens if they are not expired' do
-        FactoryBot.create_list(:invitation_token, 17)
-        allow_any_instance_of(InvitationToken).to receive(:expired?).and_return(false)
+        FactoryBot.create_list(:invitation_token, 17, created_at: 6.days.ago)
         expect(InvitationToken.count).to eq 17
         described_class.run
         expect(InvitationToken.count).to eq 17
-        expect(Response.count).to eq 17
       end
+    end
+
+    it 'should set the expiry to the response expire time if it is more than 7 days from now' do
+      measurement = FactoryBot.create(:measurement, open_duration: 10.days)
+      invitation_set = FactoryBot.create(:invitation_set)
+      FactoryBot.create(:response, open_from: 1.hour.ago,
+                                   invitation_set: invitation_set,
+                                   measurement: measurement)
+      FactoryBot.create(:invitation_token, invitation_set: invitation_set)
+      described_class.run
+      expect(InvitationToken.count).to eq 1
+      expect(InvitationToken.first.expires_at).to be_within(1.minute).
+        of(TimeTools.increase_by_duration(Time.zone.now, 10.days - 1.hour))
+    end
+    it 'should set the expiry to 7 days from now if the response expire time is less than that' do
+      measurement = FactoryBot.create(:measurement, open_duration: 6.days)
+      invitation_set = FactoryBot.create(:invitation_set)
+      FactoryBot.create(:response, open_from: 1.hour.ago,
+                                   invitation_set: invitation_set,
+                                   measurement: measurement)
+      FactoryBot.create(:invitation_token, invitation_set: invitation_set)
+      described_class.run
+      expect(InvitationToken.count).to eq 1
+      expect(InvitationToken.first.expires_at).to be_within(1.minute).
+        of(TimeTools.increase_by_duration(Time.zone.now, 7.days))
     end
   end
 end

8fc4ee4050b650039b455d700ae9978571f9d149
Frank Blaauw <frank.blaauw@gmail.com>
2018-03-07 11:27:01 +0100

    Rubocop

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Organization < ApplicationRecord
   has_many :teams, dependent: :destroy
 end
--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   sequence(:organization_name) { |n| "organization-#{n}" }
   factory :organization do
     name { generate(:organization_name) }
-    
   end
 end

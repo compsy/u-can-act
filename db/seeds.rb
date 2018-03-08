94c0207c99552188aa889b3051c855868633509c
Ando Emerencia <ando.emerencia@gmail.com>
2018-03-08 17:14:33 +0100

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,27 +20,10 @@ end
 # WARNING: seeds below are not idempotent: use dbsetup after changing something
 if Rails.env.development?
 
-  # Mentor questionnaire seeds
+  # Mentor pre assessment
   puts ''
-  protocol = Protocol.find_by_name('mentoren dagboek')
-  person = Organization.first.roles.where(group: Person::MENTOR).first.people.first
-  students = Organization.first.roles.where(group: Person::STUDENT).first.people[0..-2]
-  students.each do |student|
-    prot_sub = ProtocolSubscription.create!(
-      protocol: protocol,
-      person: person,
-      filling_out_for: student,
-      state: ProtocolSubscription::ACTIVE_STATE,
-      start_date: Time.zone.now.beginning_of_week
-    )
-    responseobj = prot_sub.responses.first
-    invitation_set = InvitationSet.create!(person: person)
-    responseobj.update_attributes!(open_from: 1.minute.ago, invitation_set: invitation_set)
-    invitation_token = invitation_set.invitation_tokens.create!
-    puts "mentor dagboek: #{Rails.application.routes.url_helpers.root_url}?q=#{person.external_identifier}#{invitation_token.token_plain}"
-  end
   protocol = Protocol.find_by_name('mentoren voormeting/nameting')
-  person = Organization.first.roles.where(group: Person::MENTOR).first.people.first
+  person = Organization.first.roles.where(group: Person::MENTOR).first.people[0]
   prot_sub = ProtocolSubscription.create!(
     protocol: protocol,
     person: person,
@@ -51,47 +34,85 @@ if Rails.env.development?
   invitation_set = InvitationSet.create!(person: person)
   responseobj.update_attributes!(open_from: 1.minute.ago, invitation_set: invitation_set)
   invitation_token = invitation_set.invitation_tokens.create!
-  puts "mentor voormeting: #{Rails.application.routes.url_helpers.root_url}?q=#{person.external_identifier}#{invitation_token.token_plain}"
+  puts "mentor voormeting: #{invitation_set.invitation_url(invitation_token.token_plain)}"
+
+  # Mentor diary
+  protocol = Protocol.find_by_name('mentoren dagboek')
+  person = Organization.first.roles.where(group: Person::MENTOR).first.people[1]
+  students = Organization.first.roles.where(group: Person::STUDENT).first.people[0..-2]
+  invitation_set = InvitationSet.create!(person: person)
+  invitation_token = invitation_set.invitation_tokens.create!
+  students.each do |student|
+    prot_sub = ProtocolSubscription.create!(
+      protocol: protocol,
+      person: person,
+      filling_out_for: student,
+      state: ProtocolSubscription::ACTIVE_STATE,
+      start_date: Time.zone.now.beginning_of_week
+    )
+    responseobj = prot_sub.responses.first
+    responseobj.update_attributes!(open_from: 1.minute.ago, invitation_set: invitation_set)
+  end
+  puts "mentor dagboek: #{invitation_set.invitation_url(invitation_token.token_plain)}"
+
+  # Mentor post assessment
+  protocol = Protocol.find_by_name('mentoren voormeting/nameting')
+  person = Organization.first.roles.where(group: Person::MENTOR).first.people[2]
+  prot_sub = ProtocolSubscription.create!(
+    protocol: protocol,
+    person: person,
+    state: ProtocolSubscription::ACTIVE_STATE,
+    start_date: Time.zone.now.beginning_of_week,
+    informed_consent_given_at: 10.minutes.ago
+  )
   responseobj = prot_sub.responses.last # nameting
+  prot_sub.responses.first.update_attributes!(open_from: 2.weeks.from_now) # make sure the voormeting is not open
   invitation_set = InvitationSet.create!(person: person)
   responseobj.update_attributes!(open_from: 1.minute.ago, invitation_set: invitation_set)
   invitation_token = invitation_set.invitation_tokens.create!
-  puts "mentor nameting: #{Rails.application.routes.url_helpers.root_url}?q=#{person.external_identifier}#{invitation_token.token_plain}"
+  puts "mentor nameting: #{invitation_set.invitation_url(invitation_token.token_plain)}"
 
-  # Student questionnaire seeds
+  # Student pre assessment
   puts ''
-  student = Organization.first.roles.where(group: Person::STUDENT).first.people.first
+  student = Organization.first.roles.where(group: Person::STUDENT).first.people[0]
   student.protocol_subscriptions.create(
     protocol: Protocol.find_by_name('studenten'),
     state: ProtocolSubscription::ACTIVE_STATE,
     start_date: Time.zone.now.beginning_of_week
   )
-  responseobj = student.protocol_subscriptions.first.responses.first
+  responseobj = student.protocol_subscriptions.first.responses.first # voormeting
+  student.protocol_subscriptions.first.responses.second.update_attributes!(open_from: 2.weeks.from_now)
   invitation_set = InvitationSet.create!(person: student)
   responseobj.update_attributes!(open_from: 1.minute.ago, invitation_set: invitation_set)
   invitation_token = invitation_set.invitation_tokens.create!
-  puts "student voormeting: #{Rails.application.routes.url_helpers.root_url}?q=#{student.external_identifier}#{invitation_token.token_plain}"
-  responseobj = student.protocol_subscriptions.first.responses[10]
-  invitation_set = InvitationSet.create!(person: student)
-  responseobj.update_attributes!(open_from: 1.minute.ago, invitation_set: invitation_set)
-  invitation_token = invitation_set.invitation_tokens.create!
-  puts "student dagboek: #{Rails.application.routes.url_helpers.root_url}?q=#{student.external_identifier}#{invitation_token.token_plain}"
-  responseobj = student.protocol_subscriptions.first.responses.last
-  invitation_set = InvitationSet.create!(person: student)
-  responseobj.update_attributes!(open_from: 1.minute.ago, invitation_set: invitation_set)
-  invitation_token = invitation_set.invitation_tokens.create!
-  puts "student nameting: #{Rails.application.routes.url_helpers.root_url}?q=#{student.external_identifier}#{invitation_token.token_plain}"
+  puts "student voormeting: #{invitation_set.invitation_url(invitation_token.token_plain)}"
 
-  puts ''
-  student = Organization.first.roles.where(group: Person::STUDENT).first.people.second
+  # Student diary
+  student = Organization.first.roles.where(group: Person::STUDENT).first.people[1]
   student.protocol_subscriptions.create(
     protocol: Protocol.find_by_name('studenten'),
     state: ProtocolSubscription::ACTIVE_STATE,
-    start_date: Time.zone.now.beginning_of_week
+    start_date: Time.zone.now.beginning_of_week,
+    informed_consent_given_at: 10.minutes.ago
+  )
+  responseobj = student.protocol_subscriptions.first.responses[10] # een dagboekmeting
+  student.protocol_subscriptions.first.responses.first.update_attributes!(open_from: 2.weeks.from_now)
+  invitation_set = InvitationSet.create!(person: student)
+  responseobj.update_attributes!(open_from: 1.minute.ago, invitation_set: invitation_set)
+  invitation_token = invitation_set.invitation_tokens.create!
+  puts "student dagboek: #{invitation_set.invitation_url(invitation_token.token_plain)}"
+
+  # Student diary almost in streak
+  student = Organization.first.roles.where(group: Person::STUDENT).first.people[2]
+  student.protocol_subscriptions.create(
+    protocol: Protocol.find_by_name('studenten'),
+    state: ProtocolSubscription::ACTIVE_STATE,
+    start_date: Time.zone.now.beginning_of_week,
+    informed_consent_given_at: 10.minutes.ago
   )
   responseobjs = student.protocol_subscriptions.first.responses
-  responseobjs[0...3].each do |response|
-    response.update_attributes!(
+  responseobjs[0...3].each do |resp|
+    resp.update_attributes!(
       open_from: (2.days.ago - 10.minutes),
       completed_at: 2.day.ago)
   end
@@ -99,8 +120,25 @@ if Rails.env.development?
   invitation_set = InvitationSet.create!(person: student)
   responseobj.update_attributes!(open_from: 1.minute.ago, invitation_set: invitation_set)
   invitation_token = invitation_set.invitation_tokens.create!
-  puts "student dagboek - Bijna in streak -: #{Rails.application.routes.url_helpers.root_url}?q=#{student.external_identifier}#{invitation_token.token_plain}"
+  puts "student dagboek (bijna in streak): #{invitation_set.invitation_url(invitation_token.token_plain)}"
 
+  # Student post assessment
+  student = Organization.first.roles.where(group: Person::STUDENT).first.people[3]
+  student.protocol_subscriptions.create(
+    protocol: Protocol.find_by_name('studenten'),
+    state: ProtocolSubscription::ACTIVE_STATE,
+    start_date: Time.zone.now.beginning_of_week,
+    informed_consent_given_at: 10.minutes.ago
+  )
+  responseobj = student.protocol_subscriptions.first.responses.last # nameting
+  voormeting = student.protocol_subscriptions.first.responses.first
+  dagboekmeting = student.protocol_subscriptions.first.responses.second
+  [voormeting, dagboekmeting].each { |resp| resp.update_attributes!(open_from: 2.weeks.from_now) }
+  invitation_set = InvitationSet.create!(person: student)
+  responseobj.update_attributes!(open_from: 1.minute.ago, invitation_set: invitation_set)
+  invitation_token = invitation_set.invitation_tokens.create!
+  puts "student nameting: #{invitation_set.invitation_url(invitation_token.token_plain)}"
+  puts ''
 
 end
 

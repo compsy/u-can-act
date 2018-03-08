4c06acff9bea8ef75b95a4652c64b172fbc3cafd
Ando Emerencia <ando.emerencia@gmail.com>
2018-03-08 20:52:25 +0100

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ require 'database_cleaner'
 require 'dotenv'
 require 'capybara/rspec'
 require 'selenium/webdriver'
-# require 'capybara-screenshot/rspec' # TODO: reenable?
+# require 'capybara-screenshot/rspec'
 Selenium::WebDriver::Chrome.driver_path = '/usr/local/bin/chromedriver' if Selenium::WebDriver::Platform.mac? &&
                                                                            File.exist?('/usr/local/bin/chromedriver')
 

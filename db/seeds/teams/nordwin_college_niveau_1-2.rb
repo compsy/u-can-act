ce6058a3f8a2e081c32f04a92132e04cd4498281
Frank Blaauw <frank.blaauw@gmail.com>
2018-03-15 08:46:59 +0100

    Also add organization on create. I think the next line is not even necessary anymore (now weve rolled out production)

--- db/seeds/teams/nordwin_college_niveau_1-2.rb
+++ db/seeds/teams/nordwin_college_niveau_1-2.rb
@@ -6,7 +6,7 @@ organization = Organization.find_by_name(organization_name)
 
 puts "Running seeds for #{team_name}"
 team = Team.find_by_name(team_name)
-team ||= Team.create!(name: team_name)
+team ||= Team.create!(name: team_name, organization: organization)
 team.update_attributes!(organization: organization)
 
 student = team.roles.where(group: Person::STUDENT).first

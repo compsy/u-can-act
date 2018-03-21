da5c1d066ec34a2a2110ee7ad9f2f6ed9618fbc9
Ando Emerencia <ando.emerencia@gmail.com>
2018-03-21 16:13:36 +0100

    added protocol transfer exporter

---
+++ spec/exporters/protocol_transfer_exporter_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ProtocolTransferExporter do
+  it_should_behave_like 'an object_exporter object'
+
+  context 'without protocol transfers' do
+    it 'works without protocol transfers' do
+      export = described_class.export_lines.to_a.join.split("\n")
+      expect(export.size).to eq 1
+    end
+  end
+
+  context 'with protocol transfers' do
+    before do
+      student = FactoryBot.create(:student)
+      mentor = FactoryBot.create(:mentor)
+      protocol_subscription = FactoryBot.create(:protocol_subscription, person: mentor, filling_out_for: student)
+      mentor2 = FactoryBot.create(:mentor, email: 'mentor2@mentor.com')
+      protocol_subscription.transfer!(mentor2)
+    end
+    it 'works with protocol transfers' do
+      export = described_class.export_lines.to_a.join.split("\n")
+      expect(export.size).to eq 2
+      # bubblebabble format for first field (person_id)
+      expect(export.last.split(';', -1).second).to match(/\A"([a-z]{5}\-){4}[a-z]{5}"\z/)
+      expect(export.last.split(';', -1).size).to eq export.first.split(';', -1).size
+    end
+    it 'filters out some numbers' do
+      student = FactoryBot.create(:student)
+      mentor = FactoryBot.create(:mentor, email: 'mentor3@mentor.com')
+      protocol_subscription = FactoryBot.create(:protocol_subscription, person: mentor, filling_out_for: student)
+      mentor2 = FactoryBot.create(:mentor,
+                                  email: 'mentor4@mentor.com',
+                                  mobile_phone: Exporters::TEST_PHONE_NUMBERS.first)
+      protocol_subscription.transfer!(mentor2)
+      export = described_class.export_lines.to_a.join.split("\n")
+      expect(export.size).to eq 2
+      # bubblebabble format for first field (person_id)
+      expect(export.last.split(';', -1).second).to match(/\A"([a-z]{5}\-){4}[a-z]{5}"\z/)
+      expect(export.last.split(';', -1).size).to eq export.first.split(';', -1).size
+    end
+  end
+end

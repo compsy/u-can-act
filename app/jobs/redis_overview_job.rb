240767781070c748f68f0e4fa06f19f212c5537d
Frank Blaauw <frank.blaauw@gmail.com>
2018-04-03 22:37:23 +0200

    Added rake task for running the job in the background

--- app/jobs/redis_overview_job.rb
+++ app/jobs/redis_overview_job.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
-class RedisOverviewJob
-  def self.run
+class RedisOverviewJob < ApplicationJob
+  queue_as :default
+
+  def perform
     Team.overview(bust_cache: true)
+  end
+
+  def max_attempts
+    2
+  end
+
+  def reschedule_at(current_time, _attempts)
+    current_time + 5.minutes
   end
 end
--- spec/jobs/redis_overview_job_spec.rb
+++ spec/jobs/redis_overview_job_spec.rb
@@ -3,10 +3,19 @@
 require 'rails_helper'
 
 describe RedisOverviewJob do
-  describe 'run' do
+  describe '#perform_later' do
+    it 'performs something later' do
+      ActiveJob::Base.queue_adapter = :test
+      expect do
+        described_class.perform_later
+      end.to have_enqueued_job(described_class)
+    end
+  end
+
+  describe 'perform' do
     it 'should call the Teamoverview function with the correct parameters' do
       expect(Team).to receive(:overview).with(bust_cache: true)
-      described_class.run
+      described_class.perform_now
     end
   end
 end

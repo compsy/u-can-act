74ffed22da7737eb6e56c8ba1db8dc6b6042563d
Frank Blaauw <frank.blaauw@gmail.com>
2018-04-05 10:04:27 +0200

    Added specs for redis overview job max / reschedule

--- spec/jobs/redis_overview_job_spec.rb
+++ spec/jobs/redis_overview_job_spec.rb
@@ -18,4 +18,18 @@ describe RedisOverviewJob do
       described_class.perform_now
     end
   end
+
+  describe 'max_attempts' do
+    it 'should be two' do
+      expect(subject.max_attempts).to eq 2
+    end
+  end
+
+  describe 'reschedule_at' do
+    it 'should be in one hour' do
+      time_now = Time.zone.now
+      expect(subject.reschedule_at(time_now, 1)).to be_within(1.minute)
+        .of(TimeTools.increase_by_duration(time_now, 5.minutes))
+    end
+  end
 end

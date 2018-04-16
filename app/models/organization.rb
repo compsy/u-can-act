3ad6aee00e9ae012e830c8d5420d905a8c63445a
Frank Blaauw <frank.blaauw@gmail.com>
2018-04-16 14:26:00 +0200

    Work in progress

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,4 +3,14 @@
 class Organization < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :teams, dependent: :destroy
+
+  def self.overview(week_number: nil, year: nil, threshold_percentage: nil, bust_cache: false)
+    overview = Organization.all.map do |organization|
+      organization_stats = organization.teams.map do |team|
+        team[:data]
+      end
+      { name: organization.name, data: organization_stats}
+    end
+    overview
+  end
 end

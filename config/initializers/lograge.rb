dc99b22c07ab0d91ad691ab4ca779db02e8ea501
Ando Emerencia <ando.emerencia@gmail.com>
2018-04-16 12:19:39 +0200

    don't give errors for teaspoon

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,6 +1,7 @@
 # Can move this to config/environments/production.rb (and staging.rb) if we only want lograge in production/staging
 Rails.application.configure do
   config.lograge.enabled = true
+  config.lograge.base_controller_class = 'ApplicationController'
   config.lograge.custom_payload do |controller|
     {
       ip: controller.request.ip,

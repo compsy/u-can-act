da33fe9d7b9a8722d1b2521bb8436cc1fa2ece83
Ando Emerencia <ando.emerencia@gmail.com>
2018-04-16 11:51:09 +0200

    make logging more silent

---
+++ config/initializers/lograge.rb
@@ -0,0 +1,17 @@
+# Can move this to config/environments/production.rb (and staging.rb) if we only want lograge in production/staging
+Rails.application.configure do
+  config.lograge.enabled = true
+  config.lograge.custom_payload do |controller|
+    {
+      ip: controller.request.ip,
+      user_id: controller.current_user.try(:id)
+    }
+  end
+  config.lograge.custom_options = lambda do |event|
+    exceptions = %w[controller action format id]
+    {
+      time: event.time,
+      params: event.payload[:params].except(*exceptions)
+    }
+  end
+end

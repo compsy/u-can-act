7e120ce387961a7fbea44fce16b64858c4d0219b
Frank Blaauw <frank.blaauw@gmail.com>
2018-04-26 15:41:03 +0200

    Use better key idification

--- db/migrate/20180423091508_add_key_to_questionnaire.rb
+++ db/migrate/20180423091508_add_key_to_questionnaire.rb
@@ -2,7 +2,7 @@ class AddKeyToQuestionnaire < ActiveRecord::Migration[5.0]
   def change
     add_column :questionnaires, :key, :string, null: true
     Questionnaire.find_each do |questionnaire|
-      questionnaire.key = questionnaire.name.parameterize.underscore
+      questionnaire.key = questionnaire.name.underscore.parameterize
       questionnaire.save! 
     end
     change_column_null :questionnaires, :key, false

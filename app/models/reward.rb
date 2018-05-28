0ba064a66eb7d5ec7f1c42209431c9f999198f83
Ando Emerencia <ando.emerencia@gmail.com>
2018-05-28 17:07:50 +0200

    cleaned up admin interface and added money earned total

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -6,4 +6,12 @@ class Reward < ApplicationRecord
   validates :protocol, presence: true
   validates_uniqueness_of :threshold, scope: :protocol_id
   belongs_to :protocol
+
+  def self.total_euros
+    students = Person.all.reject{|person| person.mentor?}
+    total_reward_points = students.reduce(0) do |total, student|
+      total + student.reward_points
+    end
+    total_reward_points / 100.0
+  end
 end

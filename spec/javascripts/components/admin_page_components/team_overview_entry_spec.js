49c32afad626137bab220c497a6932793dd9998a
Frank Blaauw <frank.blaauw@gmail.com>
2018-06-11 10:11:30 +0200

    Added specs for team overview entries

--- spec/javascripts/components/admin_page_components/team_overview_entry_spec.js
+++ spec/javascripts/components/admin_page_components/team_overview_entry_spec.js
@@ -1,46 +1,136 @@
-describe("TeamOverview", function() {
+describe("TeamOverviewEntry", function() {
   beforeEach(function() {
-    var component = React.createElement(TeamOverview, { });
-    this.rendered = TestUtils.renderIntoDocument(component)
+    this.overview = [{
+      'name': 'School naam',
+      'completed': 10,
+      'percentage_completed': 50,
+      'met_threshold_completion': 20,
+      'percentage_above_threshold': 30
+    }, {
+      'name': 'School naam1',
+      'completed': 11,
+      'percentage_completed': 51,
+      'met_threshold_completion': 21,
+      'percentage_above_threshold': 31
+    }]
+
+    this.overviewName = 'the_overview_name'
+    var component = React.createElement(TeamOverviewEntry, {
+      overview: this.overview,
+      name: this.overviewName
+
+    });
+
+    this.rendered = TestUtils.renderIntoDocument(component);
   });
 
-  describe("constructor", function() {
+  describe("generateOverviewRows", function() {
+    beforeEach(function() {
+      this.result = this.rendered.generateOverviewRows(this.overview, this.overviewName);
+    });
+
+
+    it("it should return an array", function() {
+      expect(this.result).toEqual(jasmine.any(Array))
+      expect(this.result.length).toEqual(2)
+    });
+
+    it("it should contain a table row with the correct elements and their keys and values", function() {
+      var j = 0;
+      for (var i = 0, len = this.overview.length; i < len; i++) {
+        expect(this.result[i].type).toEqual('tr');
+        expect(this.result[i].key).toEqual(this.overview[i].name + '_' + this.overviewName);
+
+        j = 0;
+        expect(this.result[i].props.children[j].type).toEqual('td');
+        expect(this.result[i].props.children[j].key).toEqual(this.overview[i].name + '_name');
+        expect(this.result[i].props.children[j].props.children).toEqual(this.overview[i].name);
+
+        j++;
+        expect(this.result[i].props.children[j].type).toEqual('td');
+        expect(this.result[i].props.children[j].key).toEqual(this.overview[i].name + '_completion');
+        expect(this.result[i].props.children[j].props.children).toEqual(this.overview[i].completed);
+
+        j++;
+        expect(this.result[i].props.children[j].type).toEqual('td');
+        expect(this.result[i].props.children[j].key).toEqual(this.overview[i].name + '_percentage_completion');
+        expect(this.result[i].props.children[j].props.children).toEqual([this.overview[i].percentage_completed, '%']);
+
+        j++;
+        expect(this.result[i].props.children[j].type).toEqual('td');
+        expect(this.result[i].props.children[j].key).toEqual(this.overview[i].name + '_met_threshold_completion');
+        expect(this.result[i].props.children[j].props.children).toEqual(this.overview[i].met_threshold_completion);
+
+        j++;
+        expect(this.result[i].props.children[j].type).toEqual('td');
+        expect(this.result[i].props.children[j].key).toEqual(this.overview[i].name + '_percentage_above_threshold');
+        expect(this.result[i].props.children[j].props.children).toEqual([this.overview[i].percentage_above_threshold, '%']);
+      }
+    });
+
   });
 
-  describe("updateTeamDetails", function() {
-    
-  });
+  describe("generateTable", function() {
+    beforeEach(function() {
+      this.rows = this.rendered.generateOverviewRows(this.overview, this.overviewName);
+      this.result = this.rendered.generateTable(this.rows);
+    });
 
-  describe("componentDidMount", function() {
-    
-  });
+    it("it should return a table", function() {
+      expect(this.result.type).toEqual('table');
+    });
 
-  describe("isDone", function() {
-    
-  });
+    it("it should return a table with the correct headers", function() {
+      expect(this.result.props.children[0].type).toEqual('thead');
+      var header = this.result.props.children[0].props.children
+      expect(header.type).toEqual('tr');
 
-  describe("setHeader", function() {
-    
-  });
+      var j = 0;
+      expect(header.props.children[j].type).toEqual('th');
+      expect(header.props.children[j].props.children).toEqual(' Team');
 
-  describe('loadTeamData', function() {
-    
-  });
-  
-  describe("handleYearChange", function() {
-    
-  });
+      j++;
+      expect(header.props.children[j].type).toEqual('th');
+      expect(header.props.children[j].props.children).toEqual(' Completed');
 
-  describe("handleWeekChange", function() {
-    
-  });
+      j++;
+      expect(header.props.children[j].type).toEqual('th');
+      expect(header.props.children[j].props.children).toEqual(' Completed percentage');
 
-  describe("renderOverview", function() {
-    
+      j++;
+      expect(header.props.children[j].type).toEqual('th');
+      expect(header.props.children[j].props.children).toEqual(' ≥ 70% completed questionnaires');
+
+      j++;
+      expect(header.props.children[j].type).toEqual('th');
+      expect(header.props.children[j].props.children).toEqual(' % of mentors with ≥ 70% completed questionnaires');
+    });
+
+    it("it should return a table with the correct body", function() {
+      expect(this.result.props.children[1].type).toEqual('tbody');
+      var body = this.result.props.children[1].props.children
+      expect(body).toEqual(this.rows);
+    });
   });
 
   describe("render", function() {
-    
-  });
+    beforeEach(function() {
+      this.rows = this.rendered.generateOverviewRows(this.overview, this.overviewName);
+      this.result = ReactDOM.findDOMNode(this.rendered)
+    });
 
+    it("it should return a div with the correct class", function() {
+      expect(this.result.nodeName).toEqual('DIV');
+      expect(this.result.attributes.class.nodeValue).toEqual('col m12 l6');
+    });
+
+    it("it should return the correct title", function() {
+      expect(this.result.children[0].nodeName).toEqual('H4')
+      expect(this.result.children[0].innerText).toEqual(this.overviewName)
+    });
+
+    it("it should return the correct table", function() {
+      expect(this.result.children[1].nodeName).toEqual('TABLE')
+    });
+  });
 });
---
+++ spec/javascripts/components/admin_page_components/team_overview_spec.js
@@ -0,0 +1,46 @@
+describe("TeamOverview", function() {
+  beforeEach(function() {
+    var component = React.createElement(TeamOverview, { });
+    this.rendered = TestUtils.renderIntoDocument(component)
+  });
+
+  describe("constructor", function() {
+  });
+
+  describe("updateTeamDetails", function() {
+    
+  });
+
+  describe("componentDidMount", function() {
+    
+  });
+
+  describe("isDone", function() {
+    
+  });
+
+  describe("setHeader", function() {
+    
+  });
+
+  describe('loadTeamData', function() {
+    
+  });
+  
+  describe("handleYearChange", function() {
+    
+  });
+
+  describe("handleWeekChange", function() {
+    
+  });
+
+  describe("renderOverview", function() {
+    
+  });
+
+  describe("render", function() {
+    
+  });
+
+});

2275bb0df4439545f3d652a3fb8ab8b86ec49c35
Frank Blaauw <frank.blaauw@gmail.com>
2018-06-14 11:15:54 +0200

    Include in class

--- app/logic/next_page_finder.rb
+++ app/logic/next_page_finder.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
-include Rails.application.routes.url_helpers
-
 class NextPageFinder
-  def self.get_next_page(current_user:, previous_response: nil, next_response: nil)
-    return previous_response.measurement.redirect_url if previous_response&.measurement&.redirect_url
+  class << self
+    include Rails.application.routes.url_helpers
 
-    next_response ||= current_user.my_open_responses.first
-    return questionnaire_path(uuid: next_response.uuid) if next_response.present?
+    def get_next_page(current_user:, previous_response: nil, next_response: nil)
+      return previous_response.measurement.redirect_url if previous_response&.measurement&.redirect_url
 
-    return mentor_overview_index_path if current_user.mentor?
+      next_response ||= current_user.my_open_responses.first
+      return questionnaire_path(uuid: next_response.uuid) if next_response.present?
 
-    klaar_path
+      return mentor_overview_index_path if current_user.mentor?
+
+      klaar_path
+    end
   end
 end

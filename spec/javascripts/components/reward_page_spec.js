a8d73e92a71afba882b49b781a883bd9f30af4de
Frank Blaauw <frank.blaauw@gmail.com>
2018-06-18 16:54:23 +0200

    Remove empty / test-tests

--- spec/javascripts/components/reward_page_spec.js
+++ spec/javascripts/components/reward_page_spec.js
@@ -1,15 +1,4 @@
 
 describe("RewardPage", function() {
-  beforeEach(function() {
-    component = React.createElement(RewardPage, 1);
-    this.rendered = TestUtils.renderIntoDocument(component)
-  });
-  
-  it("returns true if the protocol subscription is done", function() {
-    console.log(this.rendered);
-
-    done = this.rendered.isDone()
-    expect(done).toEqual(true);
-  });
 });
 
--- spec/javascripts/test_spec.js
+++
@@ -1,8 +0,0 @@
-describe("Testing", function() {
-  it("is going so smoothly", function() {
-    expect(true).toEqual(true);
-  });
-  it("is not going so smoothly", function() {
-    expect(true).toEqual(false);
-  });
-});

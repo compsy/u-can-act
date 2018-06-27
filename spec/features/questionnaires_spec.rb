edeeefe80fce86b153867e829cede3693dc1fc1b
Ando Emerencia <ando.emerencia@gmail.com>
2018-06-27 13:24:04 +0200

    change uitschrijfmessage

--- spec/features/questionnaires_spec.rb
+++ spec/features/questionnaires_spec.rb
@@ -756,7 +756,7 @@ describe 'GET and POST /', type: :feature, js: true do
       page.check('Ja', allow_label_click: true)
       page.click_on 'Opslaan'
       expect(page).to have_content('Bedankt voor het invullen van de vragenlijst!')
-      expect(page).to have_content('Succes: Je hebt je voor de dagboekstudie uitgeschreven. Bedankt voor je deelname!')
+      expect(page).to have_content('Je hebt je uitgeschreven voor het u-can-act onderzoek. Bedankt voor je inzet!')
       responseobj.reload
       expect(responseobj.completed_at).to be_within(1.minute).of(Time.zone.now)
       expect(responseobj.content).to_not be_nil
@@ -792,8 +792,7 @@ describe 'GET and POST /', type: :feature, js: true do
       # v1
       page.click_on 'Opslaan'
       expect(page).to have_content('Bedankt voor het invullen van de vragenlijst!')
-      expect(page).not_to have_content('Succes: Je hebt je voor de dagboekstudie uitgeschreven. ' \
-                                       'Bedankt voor je deelname!')
+      expect(page).not_to have_content('Je hebt je uitgeschreven voor het u-can-act onderzoek. Bedankt voor je inzet!')
       responseobj.reload
       expect(responseobj.completed_at).to be_within(1.minute).of(Time.zone.now)
       expect(responseobj.content).to_not be_nil
@@ -1131,7 +1130,7 @@ describe 'GET and POST /', type: :feature, js: true do
       page.choose('Ja', allow_label_click: true)
       page.click_on 'Opslaan'
       expect(page).to have_content('Bedankt voor het invullen van de vragenlijst!')
-      expect(page).to have_content('Succes: Je hebt je voor de dagboekstudie uitgeschreven. Bedankt voor je deelname!')
+      expect(page).to have_content('Je hebt je uitgeschreven voor het u-can-act onderzoek. Bedankt voor je inzet!')
       responseobj.reload
       expect(responseobj.completed_at).to be_within(1.minute).of(Time.zone.now)
       expect(responseobj.content).to_not be_nil
@@ -1168,8 +1167,7 @@ describe 'GET and POST /', type: :feature, js: true do
       page.choose('Nee', allow_label_click: true)
       page.click_on 'Opslaan'
       expect(page).to have_content('Bedankt voor het invullen van de vragenlijst!')
-      expect(page).not_to have_content('Succes: Je hebt je voor de dagboekstudie uitgeschreven. ' \
-                                       'Bedankt voor je deelname!')
+      expect(page).not_to have_content('Je hebt je uitgeschreven voor het u-can-act onderzoek. Bedankt voor je inzet!')
       responseobj.reload
       expect(responseobj.completed_at).to be_within(1.minute).of(Time.zone.now)
       expect(responseobj.content).to_not be_nil

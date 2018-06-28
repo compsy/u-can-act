9cdb2d99c5dda59c3569dca027c2057e10133201
Frank Blaauw <frank.blaauw@gmail.com>
2018-06-28 16:31:16 +0200

    Added organization overview to the organizations, with caching

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,14 +3,38 @@
 class Organization < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :teams, dependent: :destroy
+  ORGANIZATION_OVERVIEW_BASE_KEY = 'organization_overview'
 
   def self.overview(week_number: nil, year: nil, threshold_percentage: nil, bust_cache: false)
-    overview = Organization.all.map do |organization|
-      organization_stats = organization.teams.map do |team|
-        team[:data]
+    key = overview_key(week_number, year)
+    RedisCachedCall.cache(key, bust_cache) do
+      Organization.all.map do |organization|
+        organization_stats = organization.teams.map do |team|
+          team.stats(week_number, year, threshold_percentage)
+        end
+        organization_stats = summarize_organization_data(organization_stats)
+        { name: organization.name, data: organization_stats }
       end
-      { name: organization.name, data: organization_stats}
     end
-    overview
+  end
+
+  def self.overview_key(week_number = nil, year = nil)
+    week_number = (week_number || Time.zone.now.to_date.cweek).to_i
+    year = (year || Time.zone.now.year).to_i
+    "#{ORGANIZATION_OVERVIEW_BASE_KEY}_#{year}_#{week_number}"
+  end
+
+  def self.summarize_organization_data(organization_data)
+    result = {}
+    organization_data.each do |team|
+      team.each do |person_key, value|
+        result[person_key] = {} unless result.key? person_key
+        result[person_key].merge!(value) { |_k, total, current| total + current }
+
+        # Currently not supported
+        result[person_key].delete :percentage_above_threshold
+      end
+    end
+    result
   end
 end

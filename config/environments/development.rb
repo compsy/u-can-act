0f9f2b34c212aaee66cb810a931332fb0a88ec4d
Frank Blaauw <frank.blaauw@gmail.com>
2018-06-28 11:26:56 +0200

    Remove jasmine asset compilation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,12 +45,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  Rails.application.config.assets.precompile += [
-    'teaspoon.css',
-    'teaspoon-jasmine.js',
-    'jasmine/2.3.4.js'
-  ]
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 

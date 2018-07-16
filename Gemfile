e28f04a2a11f0547a218f7a8025b6c8ebcc6a46c
Frank Blaauw <frank.blaauw@gmail.com>
2018-07-16 12:30:31 +0200

    Added cancancan for authorization

--- Gemfile
+++ Gemfile
@@ -86,6 +86,10 @@ gem 'active_model_serializers'
 gem 'jwt'
 gem 'knock'
 
+# Gem for authorization
+# Using the 3.0 version because of this: https://github.com/CanCanCommunity/cancancan/pull/474
+gem 'cancancan', github: 'CanCanCommunity/cancancan', branch: 'feature/3.0.0'
+
 # Gem for calling deadmanssnitch
 gem 'snitcher'
 
---
+++ app/models/ability.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(person)
+    # The first argument to `can` is the action you are giving the user
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on.
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, :published => true
+    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    can :update, Person, %i[first_name last_name
+                            gender
+                            mobile_phone]
+    if person&.role&.group == Person::STUDENT
+      can :update, Person, :iban
+    elsif person&.role&.group == Person::MENTOR
+      can :update, Person, :email
+    end
+  end
+end

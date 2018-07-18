0974a58d12f3df877afde207c6ca2849b147445e
Frank Blaauw <frank.blaauw@gmail.com>
2018-07-18 16:55:43 +0200

    actually store in a variable

--- app/use_cases/calculate_earned_by_person.rb
+++ app/use_cases/calculate_earned_by_person.rb
@@ -11,7 +11,7 @@ class CalculateEarnedByPerson < ActiveInteraction::Base
     # If we, in the future, would like to include the mentoring protocol subscriptions
     # here as well (for example, if they would also receive a reward), make sure to also
     # take the protocol transfers into account, in order to generate a fair estimate.
-    person.protocol_subscriptions.select(&:for_myself?)
+    prot_subs = person.protocol_subscriptions.select(&:for_myself?)
     prot_subs.sum(&:earned_euros)
   end
 end

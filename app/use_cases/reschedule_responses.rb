ee67b8a4c44a436151e54d24572b04f96cf1bb6f
Frank Blaauw <frank.blaauw@gmail.com>
2018-07-26 10:26:37 +0200

    Pass the measurement as a variable

--- app/use_cases/reschedule_responses.rb
+++ app/use_cases/reschedule_responses.rb
@@ -26,7 +26,7 @@ class RescheduleResponses < ActiveInteraction::Base
   def schedule_responses_for_measurement(measurement)
     measurement.response_times(protocol_subscription.start_date, protocol_subscription.end_date).each do |time|
       next if current_or_past_time? time
-      next if measurement_response_completed_and_not_periodical?
+      next if measurement_response_completed_and_not_periodical? measurement
       Response.create!(protocol_subscription_id: protocol_subscription.id,
                        measurement_id: measurement.id,
                        open_from: time)
@@ -37,7 +37,7 @@ class RescheduleResponses < ActiveInteraction::Base
     time <= future
   end
 
-  def measurement_response_completed_and_not_periodical?
+  def measurement_response_completed_and_not_periodical?(measurement)
     protocol_subscription.responses.completed.where(measurement_id: measurement.id).present? &&
       !measurement.periodical?
   end

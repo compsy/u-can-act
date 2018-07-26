ca01c6711382b473d73f9705c220558723f5a44a
Frank Blaauw <frank.blaauw@gmail.com>
2018-07-26 07:02:15 +0200

    Added test and spec

--- app/use_cases/reschedule_responses.rb
+++ app/use_cases/reschedule_responses.rb
@@ -26,6 +26,8 @@ class RescheduleResponses < ActiveInteraction::Base
   def schedule_responses_for_measurement(measurement)
     measurement.response_times(protocol_subscription.start_date, protocol_subscription.end_date).each do |time|
       next if time <= future
+      next if protocol_subscription.responses.completed.where(measurement_id: measurement.id).present? && 
+        !measurement.periodical?
       Response.create!(protocol_subscription_id: protocol_subscription.id,
                        measurement_id: measurement.id,
                        open_from: time)
--- spec/use_cases/reschedule_responses_spec.rb
+++ spec/use_cases/reschedule_responses_spec.rb
@@ -75,5 +75,102 @@ describe RescheduleResponses do
       described_class.run!(protocol_subscription: protocol_subscription, future: future)
       expect(Response.count).to eq(2)
     end
+
+    describe 'rescheduling single time measurements' do
+      let(:protocol) { FactoryBot.create(:protocol, duration: 5.weeks) }
+      let!(:measurement) do
+        FactoryBot.create(:measurement, protocol: protocol,
+                                        open_from_offset: nil,
+                                        offset_till_end: 2.days + 12.hours,
+                                        period: nil,
+                                        open_duration: nil,
+                                        reward_points:  0,
+                                        stop_measurement: true,
+                                        should_invite: true,
+                                        redirect_url: '/person/edit')
+      end
+
+      let!(:protocol_subscription) do
+        FactoryBot.create(:protocol_subscription,
+                          protocol: protocol,
+                          start_date: 1.week.ago.at_beginning_of_day,
+                          end_date: 3.days.from_now)
+      end
+      before :each do
+        Timecop.freeze(2018, 7, 26)
+      end
+
+      after :each do
+        Timecop.return
+      end
+
+      it 'should reschedule not future responses for non periodical measurements if they is one completed ' do
+        # Using the student nameting as an example
+        Response.destroy_all
+
+        # Create finished response
+        finished_response = Response.create!(protocol_subscription_id: protocol_subscription.id,
+                                             measurement_id: measurement.id,
+                                             completed_at: 7.days.ago.in_time_zone,
+                                             open_from: 8.days.ago.in_time_zone)
+
+        expect_any_instance_of(Measurement).to receive(:response_times).with(
+          protocol_subscription.start_date,
+          protocol_subscription.end_date
+        ).and_call_original
+
+        expect(Response.count).to eq(1)
+        described_class.run!(protocol_subscription: protocol_subscription)
+        expect(Response.count).to eq(1)
+        expect(Response.all.first).to eq finished_response
+      end
+
+      it 'should reschedule future responses for non periodical measurements if there are none completed ' do
+        Response.destroy_all
+
+        expect_any_instance_of(Measurement).to receive(:response_times).with(
+          protocol_subscription.start_date,
+          protocol_subscription.end_date
+        ).and_call_original
+
+        expect(Response.count).to eq(0)
+        described_class.run!(protocol_subscription: protocol_subscription)
+        expect(Response.count).to eq(1)
+        expect(Response.all.first.measurement).to eq measurement
+        expect(Response.all.first.protocol_subscription).to eq protocol_subscription
+      end
+
+      fit 'should reschedule not future responses for non periodical measurements if they is one completed ' do
+        protocol.reload
+        protocol.measurements.destroy_all
+        measurement = FactoryBot.create(:measurement, :periodical, 
+                                        protocol: protocol,
+                                        open_duration: 10.weeks,
+                                        reward_points:  0,
+                                        should_invite: true,
+                                        redirect_url: '/person/edit')
+        Response.destroy_all
+
+        protocol.reload
+        expect(protocol.measurements.length).to eq 1
+
+        # Create finished response
+        finished_response = Response.create!(protocol_subscription_id: protocol_subscription.id,
+                                             measurement_id: measurement.id,
+                                             completed_at: 7.days.ago.in_time_zone,
+                                             open_from: 8.days.ago.in_time_zone)
+
+        expect_any_instance_of(Measurement).to receive(:response_times).with(
+          protocol_subscription.start_date,
+          protocol_subscription.end_date
+        ).and_call_original
+
+        expect(Response.count).to eq(1)
+        expect(protocol_subscription.responses.count).to eq(1)
+
+        described_class.run!(protocol_subscription: protocol_subscription)
+        expect(Response.count).to eq(2)
+      end
+    end
   end
 end

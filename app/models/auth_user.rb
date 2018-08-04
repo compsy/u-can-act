4bee4f9f0956253747c4fb825777bd42f9b6dbde
Frank Blaauw <frank.blaauw@gmail.com>
2018-08-04 23:08:34 +0200

    Rubocop

--- app/models/auth_user.rb
+++ app/models/auth_user.rb
@@ -11,7 +11,6 @@ class AuthUser < ApplicationRecord
   USER_ROLE = 'user'
 
   class << self
-
     ##
     # This function gets called automatically when authorizing a user. So note
     # that if we raise from here, the authorization process stops and it might

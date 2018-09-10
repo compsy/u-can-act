0ee7a8d9cc783298e37d446f8dbe323e4678846c
Frank Blaauw <frank.blaauw@gmail.com>
2018-09-10 07:16:38 +0200

    Added demography list

--- db/schema.rb
+++ db/schema.rb
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20180709070445) do
     t.string   "email"
     t.integer  "role_id",             null: false
     t.string   "external_identifier", null: false
+    t.string   "bankaccount"
     t.string   "iban"
     t.index ["mobile_phone"], name: "index_people_on_mobile_phone", unique: true, using: :btree
   end
---
+++ db/seeds/questionnaires/kct/demographykct.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+title = 'Demografische vragen'
+
+name = 'KCT Q-DEM'
+questionnaire = Questionnaire.find_by_name(name)
+questionnaire ||= Questionnaire.new(name: name)
+questionnaire.key = File.basename(__FILE__)[0...-3]
+
+content = [
+  {
+    type: :raw,
+    content: '
+    <p class="flow-text section-explanation">
+    Onderstaande informatie is van belang voor de wetenschappelijke kennis, waarbij het van
+    belang is dat we informatie hebben over het "soort" onderzoekdeelnemers (hierbij blijven
+    identificeerbare gegevens, zoals namen, volledig anoniem).
+    </p>'
+  }, {
+    id: :v1,
+    type: :textfield,
+    required: true,
+    title: 'Leeftijd',
+    placeholder: '... jaar'
+  }, {
+    id: :v2,
+    type: :radio,
+    title: 'Geslacht',
+    options: %w[Man
+                Vrouw],
+    show_otherwise: false
+  }, {
+    id: :v3,
+    type: :textfield,
+    required: true,
+    title: 'Lengte',
+    placeholder: '... meter'
+  }, {
+    id: :v4,
+    type: :textfield,
+    required: true,
+    title: 'Gewicht',
+    placeholder: '... kilogram'
+  }, {
+    id: :v5,
+    type: :radio,
+    title: 'Wat is de hoogst gevolgde opleiding die je hebt voltooid of momenteel volgt?',
+    options: %w[WO HBO MBO VWO HAVO VMBO]
+  }, {
+    id: :v6,
+    type: :textfield,
+    required: true,
+    title: 'Hoeveel uur per week besteed je gemiddeld aan sportbeoefening?',
+    placeholder: '... uur'
+  }, {
+    id: :v7,
+    type: :radio,
+    title: 'Welke rang heb je momenteel?',
+    options: [
+      'Sld',
+      'Kpl',
+      'Kpl 1',
+      'Sgt',
+      'Sgt 1',
+      'Sergeant majoor',
+      'Adjudant',
+      'Luitenant',
+      'Kapitein',
+      'Majoor',
+      'Luitenant kolonel',
+      'Kolonel'
+    ]
+  }, {
+    id: :v8,
+    type: :radio,
+    title: 'Ik ben op dit moment in opleiding als:',
+    options: [
+      'VO / ECO',
+      'VCO'
+    ]
+  }, {
+    id: :v9,
+    type: :radio,
+    title: 'Ik ben werkzaam bij:',
+    options: [
+      'de staf',
+      '102 COTRCIE',
+      '103 COTRCIE',
+      '104 COTRCIE',
+      '105 COTRCIE',
+      '108 COTRCIE',
+      'OTCSO'
+    ]
+  }, {
+    id: :v10,
+    type: :textfield,
+    required: true,
+    title: 'Hoeveel jaar ben je in totaal werkzaam bij Defensie?',
+    placeholder: '... jaar'
+  }, {
+    id: :v11,
+    type: :textfield,
+    required: true,
+    title: 'Hoeveel jaar ben je werkzaam bij het KCT?',
+    placeholder: '... uur'
+  }, {
+    id: :v12,
+    type: :textfield,
+    required: true,
+    title: 'Hoeveel uitzendingen heb je inmiddels gedaan?',
+    placeholder: ''
+  }
+]
+questionnaire.content = content
+questionnaire.title = title
+questionnaire.save!

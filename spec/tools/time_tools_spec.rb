8632e01153d9644ecd6ed7a9088feb59f9c86b8d
Frank Blaauw <frank.blaauw@gmail.com>
2018-09-28 10:55:04 +0200

    Merged and rubocopped

--- spec/tools/time_tools_spec.rb
+++ spec/tools/time_tools_spec.rb
@@ -45,7 +45,7 @@ describe TimeTools do
       time = DateTime.new(2012, 8, 29, 22, 35, 0)
       expect(described_class.a_time?(time)).to be_truthy
     end
-    # rubocop:enable Style/DateTime
+
     it 'should not see anything else as a time' do
       expect(described_class.a_time?('ando')).to be_falsey
       expect(described_class.a_time?(42)).to be_falsey
@@ -76,6 +76,5 @@ describe TimeTools do
       expect(described_class.an_offset?(Time.new(2002))).to be_falsey
       expect(described_class.an_offset?(DateTime.new(2012, 8, 29, 22, 35, 0))).to be_falsey
     end
-    # rubocop:enable Style/DateTime
   end
 end

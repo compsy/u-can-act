2b392f7f14289c69d27a631027a4db39573f2700
Ando Emerencia <ando.emerencia@gmail.com>
2018-10-05 18:14:49 +0200

    fixed specs

--- spec/support/ui_macros.rb
+++ spec/support/ui_macros.rb
@@ -8,7 +8,6 @@ module UiMacros
   end
 
   def materialize_select(prompt, option, superelement = nil)
-    sleep(1)
     base = 'div.select-wrapper'
     base = superelement + base if superelement.present?
 
@@ -17,6 +16,8 @@ module UiMacros
     # 3. Go to the parent div
     # 4. Click on it to open the dropdown
     find_all("#{base}>ul>li", text: prompt).first.find(:xpath, '..').find(:xpath, '..').click
+    # Add some delay because the thing folds out slowly
+    sleep(1)
     find("#{base} li", text: option).click # select the option wanted
   end
 end

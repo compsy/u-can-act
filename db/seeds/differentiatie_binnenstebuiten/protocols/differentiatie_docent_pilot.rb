6a59162b246eff35e7841ab158a98b5b2db6a700
Frank Blaauw <frank.blaauw@gmail.com>
2018-10-07 22:29:44 +0200

    Added pilots

---
+++ db/seeds/differentiatie_binnenstebuiten/protocols/differentiatie_docent_pilot.rb
@@ -0,0 +1,31 @@
+default_protocol_duration = 15.hours + 30.minutes
+default_open_duration = 55.minutes
+default_posttest_open_duration = nil
+default_reward_points = 100
+
+###############
+## docenten ##
+###############
+
+pr_name = 'differentiatie_docenten'
+protocol = Protocol.find_by_name(pr_name)
+protocol ||= Protocol.new(name: pr_name)
+protocol.duration = default_protocol_duration
+protocol.invitation_text ='Er staat een nieuw dagboek voor je klaar. Klik op de volgende link om deze in te vullen. Alvast bedankt!'
+protocol.save!
+
+# Add dagboekmetingen
+db_name = 'differentiatie docenten'
+of_offset = 13.hours # 9 o'clock on monday morning
+dagboekvragenlijst_id = Questionnaire.find_by_name(db_name)&.id
+raise "Cannot find questionnaire: #{db_name}" unless dagboekvragenlijst_id
+db_measurement = protocol.measurements.where(questionnaire_id: dagboekvragenlijst_id,
+                                             open_from_offset: of_offset).first
+db_measurement ||= protocol.measurements.build(questionnaire_id: dagboekvragenlijst_id)
+db_measurement.open_from_offset = of_offset
+db_measurement.period = 1.hour
+db_measurement.open_duration = default_open_duration
+db_measurement.reward_points = default_reward_points
+db_measurement.stop_measurement = false
+db_measurement.should_invite = true
+db_measurement.save!

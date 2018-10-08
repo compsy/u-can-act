5ebc79bdaedce5de431218177aaa489bad022f97
Ando Emerencia <ando.emerencia@gmail.com>
2018-10-08 14:49:59 +0200

    add webpacker

---
+++ config/webpack/custom.js
@@ -0,0 +1,11 @@
+module.exports = {
+  resolve: {
+    alias: {
+      jquery: 'jquery/src/jquery',
+      vue: 'vue/dist/vue.js',
+      React: 'react',
+      ReactDOM: 'react-dom',
+      vue_resource: 'vue-resource/dist/vue-resource',
+    }
+  }
+}

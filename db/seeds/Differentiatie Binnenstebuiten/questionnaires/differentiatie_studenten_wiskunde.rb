dc0bb71d8dc262c2aaa420ea8543823b974bdaee
Frank Blaauw <frank.blaauw@gmail.com>
2018-10-16 08:40:22 +0200

    Capitalize wiskunde

--- db/seeds/Differentiatie Binnenstebuiten/questionnaires/differentiatie_studenten_wiskunde.rb
+++ db/seeds/Differentiatie Binnenstebuiten/questionnaires/differentiatie_studenten_wiskunde.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 db_title = '' # Dagboekvragenlijst moet geen titel hebben alleen een logo
 
-db_name1 = 'Differentiatie Binnenstebuiten Scholieren wiskunde'
+db_name1 = 'Differentiatie Binnenstebuiten Scholieren Wiskunde'
 dagboek1 = Questionnaire.find_by_name(db_name1)
 dagboek1 ||= Questionnaire.new(name: db_name1)
 dagboek1.key = File.basename(__FILE__)[0...-3]

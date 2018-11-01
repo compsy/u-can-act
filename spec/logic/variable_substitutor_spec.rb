2de19eaa392d5d120cd35d711c95e980ba7a5dfb
Ando Emerencia <ando.emerencia@gmail.com>
2018-11-01 13:39:21 +0100

    added specs for variable evaluator, variable substitutor, and textfield generator

--- spec/logic/variable_substitutor_spec.rb
+++ spec/logic/variable_substitutor_spec.rb
@@ -7,8 +7,14 @@ describe VariableSubstitutor do
     let(:team) { FactoryBot.create(:team) }
     let(:student_role) { FactoryBot.create(:role, team: team, group: Person::STUDENT, title: Person::STUDENT) }
     let(:mentor_role) { FactoryBot.create(:role, team: team, group: Person::MENTOR, title: 'MentorTitle') }
-    let(:student) { FactoryBot.create(:student, role: student_role, first_name: 'Emma', gender: Person::FEMALE) }
-    let(:mentor) { FactoryBot.create(:mentor, role: mentor_role, first_name: 'Pieter', gender: Person::MALE) }
+    let(:student) do
+      FactoryBot.create(:student, role: student_role, first_name: 'Emma', last_name: 'Bergsma',
+                        gender: Person::FEMALE)
+    end
+    let(:mentor) do
+      FactoryBot.create(:mentor, role: mentor_role, first_name: 'Pieter', last_name: 'Takens',
+                        gender: Person::MALE)
+    end
 
     describe 'substitute_variables' do
       let!(:prot_ment) { FactoryBot.create(:protocol_subscription, person: mentor, filling_out_for: student) }
@@ -34,8 +40,10 @@ describe VariableSubstitutor do
           expect(result[:mentor_title]).to eq mentor.role.title
           expect(result[:mentor_gender]).to eq mentor.gender
           expect(result[:mentor_name]).to eq mentor.first_name
+          expect(result[:mentor_last_name]).to eq mentor.last_name
           expect(result[:organization]).to eq student.role.team.organization.name
           expect(result[:student_name]).to eq student.first_name
+          expect(result[:student_last_name]).to eq student.last_name
           expect(result[:student_gender]).to eq student.gender
         end
       end
@@ -53,8 +61,10 @@ describe VariableSubstitutor do
           expect(result[:mentor_title]).to eq mentor.role.title
           expect(result[:mentor_gender]).to eq mentor.gender
           expect(result[:mentor_name]).to eq mentor.first_name
+          expect(result[:mentor_last_name]).to eq mentor.last_name
           expect(result[:organization]).to eq student.role.team.organization.name
           expect(result[:student_name]).to eq student.first_name
+          expect(result[:student_last_name]).to eq student.last_name
           expect(result[:student_gender]).to eq student.gender
         end
       end

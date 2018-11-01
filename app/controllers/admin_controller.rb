e74a55ddbffbbeb1c715c5a584018ef524f1cdb7
Ando Emerencia <ando.emerencia@gmail.com>
2018-11-01 13:40:27 +0100

    added todo

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,6 +8,7 @@ class AdminController < ApplicationController
   before_action :load_questionnaires, only: %i[export preview_overview]
 
   def preview
+    # TODO: Add a migration to read the layout from the questionnaire database model.
     @use_mentor_layout = @questionnaire.name.match?(/mentor|evaluatieonderzoek|telefonische interviews/)
   end
 

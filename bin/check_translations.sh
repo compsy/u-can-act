6eae4c4d468b733c0798ef5c246bd9fa5c1d02ad
Ando Emerencia <ando.emerencia@gmail.com>
2019-01-07 13:58:08 +0100

    add check for checking if translations.js is updated

---
+++ bin/check_translations.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+oldfile=public/javascripts/translations_old.js
+newfile=public/javascripts/translations.js
+git checkout -- $newfile
+cp $newfile $oldfile
+bundle exec rake i18n:js:export
+files_are_different=$(cmp $oldfile $newfile)
+rm -f $oldfile
+if [ "$files_are_different" != "" ]; then
+	echo files are different
+	exit 1
+fi

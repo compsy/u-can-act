326b9b20284db3cbfeb80f276e3c9618c27ef315
Frank Blaauw <frank.blaauw@gmail.com>
2019-04-16 16:12:34 +0200

    Remove useless route

--- config/routes.rb
+++ config/routes.rb
@@ -47,7 +47,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :questionnaire, only: [:index, :show, :create], param: :key 
+      resources :questionnaire, only: [:show, :create], param: :key 
       resources :response, only: [:show, :index, :create], param: :uuid
       resources :person do
         collection do

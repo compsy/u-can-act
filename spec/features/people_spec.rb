b64f25e989d8a9915622647d6e1ed09dfc4ad1cd
Ando Emerencia <ando.emerencia@gmail.com>
2019-05-15 15:57:29 +0200

    fixed specs

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -155,8 +155,6 @@ describe 'GET /edit', type: :feature, js: true do
       responseobj.complete!
       page.fill_in('person_email', with: 'anew@email.com')
       all('button[type="submit"]').first.click
-      expect(page)
-        .to have_content('Uw gegevens zijn opgeslagen.')
       expect(page).to have_content('Gegevens opgeslagen')
       expect(page).to_not have_content('Disclaimer')
       expect(page).to_not have_content('Gegevens aanpassen')

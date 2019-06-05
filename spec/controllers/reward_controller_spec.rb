9f033084a09b3d953a873afd4955450a9c20ed4a
Ando Emerencia <ando.emerencia@gmail.com>
2019-06-05 14:54:24 +0200

    fixed more specs

--- spec/controllers/reward_controller_spec.rb
+++ spec/controllers/reward_controller_spec.rb
@@ -2,9 +2,9 @@
 
 require 'rails_helper'
 
-RSpec.describe RewardController, type: :controller do
+describe RewardController, type: :controller do
   render_views
-  it_behaves_like 'an is_logged_in concern', :index
+  it_behaves_like 'an is_logged_in concern', :index, {}
   describe 'GET /klaar' do
     let(:student) { FactoryBot.create(:student) }
 

efee332072a0d562b869dd05e5eb3486c896494e
Ando Emerencia <ando.emerencia@gmail.com>
2019-06-05 15:14:30 +0200

    replace instances of file.join by rails.root.join

--- spec/controllers/api/v1/settings_controller_spec.rb
+++ spec/controllers/api/v1/settings_controller_spec.rb
@@ -11,9 +11,9 @@ describe Api::V1::SettingsController, type: :controller do
       res
     end
 
-    let(:settings) { YAML.load_file(File.join(Rails.root, 'config', 'settings.yml')) }
+    let(:settings) { YAML.load_file(Rails.root.join('config', 'settings.yml')) }
     let(:specific_settings) do
-      YAML.load_file(File.join(Rails.root, 'projects', 'demo', 'config', 'settings.yml'))
+      YAML.load_file(Rails.root.join('projects', 'demo', 'config', 'settings.yml'))
     end
 
     let(:current_settings) { settings[Rails.env].deep_merge(specific_settings[Rails.env]) }

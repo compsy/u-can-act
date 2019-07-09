280aad5f024d150e8a8eb8dc4224cfde64a524d0
Ando Emerencia <ando.emerencia@gmail.com>
2019-07-09 18:19:31 +0200

    remove admin model

--- spec/factories/auth_users.rb
+++ spec/factories/auth_users.rb
@@ -4,15 +4,15 @@ FactoryBot.define do
   sequence(:auth0_id_string) { |n| "id_#{format('%10d', n)}" }
   factory :auth_user do
     auth0_id_string
-    password_digest 'thepasswordwhicisrandom'
-    role AuthUser::USER_ROLE
-    person nil
+    password_digest { 'thepasswordwhicisrandom' }
+    role { AuthUser::USER_ROLE }
+    person { nil }
 
     trait :with_person do
       person
     end
     trait :admin do
-      role AuthUser::ADMIN_ROLE
+      role { AuthUser::ADMIN_ROLE }
     end
   end
 end

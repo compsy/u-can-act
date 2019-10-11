f7450e203f305c7bb25e0a6451dc943aa5db2645
Rik <rik@huijzer.xyz>
2019-10-11 15:23:55 +0200

    Repeat maandag every day

--- projects/kct/seeds/protocols/kct.rb
+++ projects/kct/seeds/protocols/kct.rb
@@ -25,7 +25,7 @@ questionnaires.each do |name|
   raise "Cannot find questionnaire: #{name}" unless dagboekvragenlijst_id
   db_measurement = protocol.measurements.where(questionnaire_id: dagboekvragenlijst_id).first
   db_measurement ||= protocol.measurements.build(questionnaire_id: dagboekvragenlijst_id)
-  db_measurement.period = nil
+  db_measurement.period = 1.days
   db_measurement.open_duration = default_open_duration
   db_measurement.open_from_offset = 0
   db_measurement.reward_points = default_reward_points

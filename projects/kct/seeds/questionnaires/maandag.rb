777118cf46192bbe3bd1033643c6066dd5943135
Rik <rik@huijzer.xyz>
2019-10-14 10:29:53 +0200

    Update maandag based on feedback Ruud

--- projects/kct/seeds/questionnaires/maandag.rb
+++ projects/kct/seeds/questionnaires/maandag.rb
@@ -13,47 +13,120 @@ content = [
     content: '
     <p class="flow-text section-explanation">
     Wil je hieronder zo eerlijk en nauwkeurig mogelijk aangeven hoe jij je op dit moment voelt?
-
-    <table>
-      <tr><td>6</td><td></td></tr>
-      <tr><td>7</td><td>heel, heel licht inspannend</td></tr>
-      <tr><td>8</td><td></td></tr>
-      <tr><td>9</td><td>heel licht inspannend</td></tr>
-      <tr><td>10</td><td></td></tr>
-      <tr><td>11</td><td>licht inspannend</td></tr>
-      <tr><td>12</td><td></td></tr>
-      <tr><td>13</td><td>redelijk inspannend</td></tr>
-      <tr><td>14</td><td></td></tr>
-      <tr><td>15</td><td>inspannend</td></tr>
-      <tr><td>16</td><td></td></tr>
-      <tr><td>17</td><td>heel inspannend</td></tr>
-      <tr><td>18</td><td></td></tr>
-      <tr><td>19</td><td>heel, heel inspannend</td></tr>
-      <tr><td>20</td><td></td></tr>
-    </table>
-
-    </p>'
+    '
   },
   {
     id: :v1,
     type: :radio,
     required: true,
     title: 'Hoe inspannend was deze week voor jou?',
-    options: %w[6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    options: [
+      '6',
+      '7 (heel, heel licht inspannend)',
+      '8',
+      '9 (heel licht inspannend)',
+      '10',
+      '11 (licht inspannend)',
+      '12',
+      '13 (redelijk inspannend)',
+      '14',
+      '15 (inspannend)',
+      '16',
+      '17 (heel inspannend)',
+      '18',
+      '19 (heel, heel inspannend)',
+      '20'
+    ],
     show_otherwise: false,
   },
   {
     id: :v2,
     type: :radio,
     title: 'Hoe vermoeid voel je je op dit moment?',
-    options: ['Helemaal niet vermoeid', '2', '3', '4', '5', '6', 'Heel erg vermoeid'],
+    options: [
+      '1 (helemaal niet vermoeid)',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7 (heel erg vermoeid)'
+    ],
     show_otherwise: false,
   },
   {
     id: :v3,
     type: :radio,
     title: 'Hoe heb je de afgelopen nachten geslapen?',
-    options: ['Heel erg slecht', '2', '3', '4', '5', '6', 'Heel erg goed'],
+    options: [
+      '1 (heel erg slecht)',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7 (heel erg goed)'
+    ],
+    show_otherwise: false,
+  },
+  {
+    id: :v4,
+    type: :radio,
+    title: 'Hoeveel spierpijn heb je op dit moment?',
+    options: [
+      '1 (helemaal geen spierpijn)',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7 (heel veel spierpijn)'
+    ],
+    show_otherwise: false,
+  },
+  {
+    id: :v5,
+    type: :radio,
+    title: 'Hoe gestrest voel je je op dit moment?',
+    options: [
+      '1 (helemaal niet gestrest)',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7 (heel erg gestrest)'
+    ],
+    show_otherwise: false,
+  },
+  {
+    id: :v6,
+    type: :radio,
+    title: 'Hoe positief is je stemming op dit moment?',
+    options: [
+      '1 (helemaal niet positief)',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7 (heel erg positief)'
+    ],
+    show_otherwise: false,
+  },
+  {
+    id: :v7,
+    type: :radio,
+    title: 'Hoe negatief is je stemming op dit moment?',
+    options: [
+      '1 (helemaal niet negatief)',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7 (heel erg negatief)'
+    ],
     show_otherwise: false,
   },
 ]

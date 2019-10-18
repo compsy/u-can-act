209370670135a2b39555113ea5b58c0f8609ffc4
Rik <rik@huijzer.xyz>
2019-10-18 09:08:14 +0200

    Reduce protocol period

--- projects/kct/seeds/protocols/kct.rb
+++ projects/kct/seeds/protocols/kct.rb
@@ -1,6 +1,6 @@
 # ECO is 8 weeks, so we do 10 to be sure.
-default_protocol_duration = 10.weeks  # evt eerder dynamisch afbreken
-default_open_duration = 10.weeks
+default_protocol_duration = 10.weeks
+default_open_duration = 5.days # Open duration for each questionnaire. Do not make it one day.
 default_posttest_open_duration = nil
 default_reward_points = 100
 
@@ -28,10 +28,12 @@ questionnaires.each do |name|
   db_measurement = protocol.measurements.where(questionnaire_id: dagboekvragenlijst_id).first
   db_measurement ||= protocol.measurements.build(questionnaire_id: dagboekvragenlijst_id)
   # Goal is invisible after filling in, and ALWAYS visible after a week.
+  # This is a bit messy, we will have that some questionnaire is not filled in in time.
   db_measurement.period = 5.days
   db_measurement.open_duration = default_open_duration
   db_measurement.open_from_offset = 0
   db_measurement.reward_points = default_reward_points
+  # Setting to true will make this questionnaire the last for the user, cancelling protocol_duration.
   db_measurement.stop_measurement = false
   db_measurement.should_invite = true
   db_measurement.save!

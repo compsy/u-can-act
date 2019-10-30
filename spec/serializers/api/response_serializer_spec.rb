8b6893ad635dce22a8d7da44ab9847a7eb046868
Ando Emerencia <ando.emerencia@gmail.com>
2019-10-30 13:00:48 +0100

    expose responses to base platform

--- spec/serializers/api/response_serializer_spec.rb
+++ spec/serializers/api/response_serializer_spec.rb
@@ -3,9 +3,8 @@
 require 'rails_helper'
 
 describe Api::ResponseSerializer do
+  let(:response) { FactoryBot.create(:response, :completed, opened_at: 10.minutes.ago) }
   subject(:json) { described_class.new(response).as_json.with_indifferent_access }
-
-  let(:response) { FactoryBot.create(:response) }
 
   it 'contains the correct value for the uuid' do
     expect(response.uuid).not_to be_blank
@@ -18,5 +17,26 @@ describe Api::ResponseSerializer do
       .and_call_original
       .twice
     described_class.new(response).as_json
+  end
+
+  it 'contains the correct value for the open_from' do
+    expect(response.open_from).not_to be_blank
+    expect(json['open_from']).to eq response.open_from
+  end
+
+  it 'contains the correct value for the opened_at' do
+    expect(response.opened_at).not_to be_blank
+    expect(json['opened_at']).to eq response.opened_at
+  end
+
+  it 'contains the correct value for the completed_at' do
+    expect(response.completed_at).not_to be_blank
+    expect(json['completed_at']).to eq response.completed_at
+  end
+
+  it 'contains the correct value for the values' do
+    allow(response).to receive(:values).and_return({'v1' => 5, 'v2' => 'yes'})
+    expect(response.values).not_to be_blank
+    expect(json['values']).to eq response.values
   end
 end

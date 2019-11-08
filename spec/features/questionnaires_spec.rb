b3a30b8bfcaf6a310622c426f9de809b0a3871ad
Ando Emerencia <ando.emerencia@gmail.com>
2019-11-08 12:36:23 +0100

    Update questionnaires_spec.rb

--- spec/features/questionnaires_spec.rb
+++ spec/features/questionnaires_spec.rb
@@ -86,7 +86,7 @@ describe 'GET and POST /', type: :feature, js: true do
     sleep(1)
     page.click_on 'Opslaan'
     # expect(page).to have_http_status(200)
-    sleep(1)
+    sleep(5)
     expect(page).to have_content('Bedankt voor het invullen van de vragenlijst!')
     responseobj.reload
     expect(responseobj.completed_at).to be_within(1.minute).of(Time.zone.now)

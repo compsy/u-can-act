0fc0ecd638b97b5d9bd0f40fb0621e7f04680e87
Ando Emerencia <ando.emerencia@gmail.com>
2019-11-08 15:52:10 +0100

    fix output path

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,7 +70,7 @@ RSpec.configure do |config|
     timestamp = "#{time_now.strftime('%Y-%m-%d-%H-%M-%S.')}#{'%03d' % (time_now.usec / 1000).to_i}"
 
     screenshot_name = "screenshot-#{filename}-#{line_number}-#{timestamp}.png"
-    screenshot_path = "#{ENV.fetch('CIRCLE_ARTIFACTS', Rails.root.join('tmp', 'test-results'))}/#{screenshot_name}"
+    screenshot_path = "/tmp/test-results/#{screenshot_name}"
 
     page.save_screenshot(screenshot_path)
 

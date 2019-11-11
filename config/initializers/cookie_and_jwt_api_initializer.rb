35c3e631a888f3ac220a3dcda7c40a61b9b7eb4b
Ando Emerencia <ando.emerencia@gmail.com>
2019-11-11 12:36:37 +0100

    fix specs

---
+++ config/initializers/cookie_and_jwt_api_initializer.rb
@@ -0,0 +1,6 @@
+module Api
+  module V1
+    module CookieAndJwtApi
+    end
+  end
+end
--- spec/features/questionnaires_spec.rb
+++ spec/features/questionnaires_spec.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
-module Api
-  module V1
-    module CookieAndJwtApi
-    end
-  end
-end
-
 require 'rails_helper'
 
 describe 'GET and POST /', type: :feature, js: true do

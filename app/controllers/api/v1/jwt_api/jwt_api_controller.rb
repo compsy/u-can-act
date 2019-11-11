0c0481bc70b537d91c6b245e09ae85672b60975f
Frank Blaauw <frank.blaauw@gmail.com>
2019-11-11 21:42:34 +0100

    Fixed ando comments

--- app/controllers/api/v1/jwt_api/jwt_api_controller.rb
+++ app/controllers/api/v1/jwt_api/jwt_api_controller.rb
@@ -5,6 +5,20 @@ module Api
     module JwtApi
       class JwtApiController < ApiController
         include ::Concerns::IsJwtAuthenticated
+        before_action :set_person
+
+        private
+
+        def set_person
+          # Debugging
+          # token = request.headers['Authorization'].split.last
+          # Rails.logger.warn Knock::AuthToken.new(token: token)
+          # Rails.logger.warn current_auth_user.person.inspect
+          return if current_auth_user&.person.present?
+
+          result = { result: current_auth_user.to_json }
+          render(status: :not_found, json: result)
+        end
       end
     end
   end

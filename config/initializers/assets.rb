6a894a8b67149026d07f94dbc050a7009101845d
Ando Emerencia <ando.emerencia@gmail.com>
2019-11-11 17:56:02 +0100

    fix favicons to resolve and use the asset pipeline instead of hanging around in the public directory

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('node_modules').to_s
 
 if ENV['PROJECT_NAME'].nil?
   raise "\n
@@ -35,4 +35,3 @@ end
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-

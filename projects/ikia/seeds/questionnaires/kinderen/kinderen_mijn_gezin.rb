e1a5a2c3b17f9a9819b17a8a9008a9fd27dd288b
Ando Emerencia <ando.emerencia@gmail.com>
2019-11-18 13:06:12 +0100

    fix width

--- projects/ikia/seeds/questionnaires/kinderen/kinderen_mijn_gezin.rb
+++ projects/ikia/seeds/questionnaires/kinderen/kinderen_mijn_gezin.rb
@@ -6,7 +6,7 @@ db_name1 = 'Mijn_gezin_Kinderen'
 dagboek1 = Questionnaire.find_by_key(File.basename(__FILE__)[0...-3])
 dagboek1 ||= Questionnaire.new(key: File.basename(__FILE__)[0...-3])
 dagboek1.name = db_name1
-style = 'style="max-height: 200px; vertical-align: middle"'
+style = 'style="max-height: 200px; vertical-align: middle; max-width: 100%"'
 betrokkenheid1 = "<img src=\"https://u-can-act.nl/wp-content/uploads/2019/10/Betrokkenheid5.png\" #{style}>"
 betrokkenheid2 = "<img src=\"https://u-can-act.nl/wp-content/uploads/2019/10/Betrokkenheid4.png\" #{style}>"
 betrokkenheid3 = "<img src=\"https://u-can-act.nl/wp-content/uploads/2019/10/Betrokkenheid3.png\" #{style}>"

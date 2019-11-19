e1f2e0caf0a921b109c6b5c25f83d9bc1ed9240f
Ando Emerencia <ando.emerencia@gmail.com>
2019-11-19 19:46:45 +0100

    fix typo

--- spec/factories/push_subscriptions.rb
+++ spec/factories/push_subscriptions.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   sequence(:push_subscription_name) { |n| "push-subscription-#{n}" }
   factory :push_subscription do
     name { generate(:push_subscription_name) }
-    url { 'http://web:2000/api/v1/data/create_raw' }
+    url { 'http://web:3000/api/v1/data/create_raw' }
     add_attribute(:method) { 'POST' }
     protocol
   end

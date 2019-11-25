3c53b1b0a31cdfb6be1c655a8f16b609afe2a53c
Ando Emerencia <ando.emerencia@gmail.com>
2019-11-25 13:23:34 +0100

    add target audience to email registration

--- app/use_cases/email_registration.rb
+++ app/use_cases/email_registration.rb
@@ -8,9 +8,11 @@ class EmailRegistration < ActiveInteraction::Base
   # Params:
   # - person: the person to send the email to
   def execute
+    target_audience = person.role.title
+    registration_url = "#{Rails.application.config.settings.registration.url}?targetAudience=#{target_audience}"
     mailer = InvitationMailer.registration_mail(person.email,
                                                 Rails.application.config.settings.registration.text,
-                                                Rails.application.config.settings.registration.url)
+                                                registration_url)
     mailer.deliver_now
   end
 end

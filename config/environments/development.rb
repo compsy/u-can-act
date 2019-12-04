a367dc4b970daa265b24a8f30180609b00b4916c
Ando Emerencia <ando.emerencia@gmail.com>
2019-12-04 18:48:19 +0100

    use polling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,8 +50,8 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
- 
+  config.file_watcher = ActiveSupport::FileUpdateChecker
+
   # config.action_mailer.smtp_settings = {
   #   address: 'localhost',
   #   port: '1025',

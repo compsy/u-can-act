dfbcb7d329000cee22f12a4bf05f40aacbe8630b
Ando Emerencia <ando.emerencia@gmail.com>
2019-12-19 17:33:34 +0100

    fix seeds deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191119145741) do
+ActiveRecord::Schema.define(version: 20191219145741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -160,7 +160,7 @@ ActiveRecord::Schema.define(version: 20191119145741) do
     t.string "method", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_push_subscriptions_on_name", unique: true
+    t.index ["protocol_id", "name"], name: "index_push_subscriptions_on_protocol_id_and_name", unique: true
     t.index ["protocol_id"], name: "index_push_subscriptions_on_protocol_id"
   end
 

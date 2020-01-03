69b91db402aa13c1e8944c8bc4924f3a34656608
Frank Blaauw <frank.blaauw@gmail.com>
2020-01-03 12:04:36 +0100

    Add responses to person serializer

--- app/serializers/api/person_serializer.rb
+++ app/serializers/api/person_serializer.rb
@@ -3,6 +3,10 @@
 module Api
   class PersonSerializer < ActiveModel::Serializer
     type 'people'
-    attributes :first_name, :last_name, :gender, :email, :mobile_phone, :iban, :id, :account_active
+    attributes :first_name, :last_name, :gender, :email, :mobile_phone, :iban, :id, :account_active, :my_open_responses
+
+    def my_open_responses
+      object.my_open_responses(for_myself = true)
+    end
   end
 end

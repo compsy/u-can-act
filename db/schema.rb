1363fb3c1de599a93370c88dadd657f4dd17a2b6
Ando Emerencia <ando.emerencia@gmail.com>
2020-01-13 15:46:35 +0100

    fixed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191219145741) do
+ActiveRecord::Schema.define(version: 20200112233900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

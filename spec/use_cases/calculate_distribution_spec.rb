44e5f0d32a74a5b57207fdee5e3acf8a2e990131
Ando Emerencia <ando.emerencia@gmail.com>
2020-01-14 20:08:15 +0100

    fixed specs

--- spec/use_cases/calculate_distribution_spec.rb
+++ spec/use_cases/calculate_distribution_spec.rb
@@ -2,8 +2,23 @@
 
 require 'rails_helper'
 
-fdescribe CalculateDistribution do
+describe CalculateDistribution do
   describe 'execute' do
     let!(:questionnaire) { FactoryBot.create(:questionnaire) }
+
+    it 'should store the correct results' do
+      reponse_content = FactoryBot.create(:response_content, content: {'v3' => '68'})
+      response = FactoryBot.create(:response, :completed)
+      response.content = reponse_content.id
+      response.save!
+      expected = {'v3' => {}}
+      (0..100).each do |val|
+        expected['v3'][val.to_s] = 0
+      end
+      expected['v3']['68'] = 1
+      expect(RedisService).to receive(:set).with("distribution_#{response.measurement.questionnaire.key}",
+                                                 expected.to_json)
+      expect(described_class.run!(questionnaire: response.measurement.questionnaire))
+    end
   end
 end

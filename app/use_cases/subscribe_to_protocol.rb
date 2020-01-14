83120e979e01bdb09e5b1482efbc0b888717473b
Ando Emerencia <ando.emerencia@gmail.com>
2020-01-14 17:23:35 +0100

    don't set parent as mentor

--- app/use_cases/subscribe_to_protocol.rb
+++ app/use_cases/subscribe_to_protocol.rb
@@ -21,32 +21,26 @@ class SubscribeToProtocol < ActiveInteraction::Base
   # - protocol: the protocol to subscribe the person to (optional)
   # - person: the person to start the protocol for
   # - start_date: the date when the subscription should start
-  # rubocop:disable Metrics/AbcSize
   def execute
-    the_mentor = mentor
     the_protocol = find_protocol
     the_start_date = find_start_date
     Rails.logger.warn("Protocol #{the_protocol.id} does not have any measurements") if the_protocol.measurements.blank?
 
-    # If we are a child, our parent is our mentor
-    the_mentor = person.parent if the_mentor.blank? && person&.parent.present?
-
-    create_or_find_protocol_subscription(the_protocol, the_start_date, the_mentor)
+    create_or_find_protocol_subscription(the_protocol, the_start_date)
   end
 
-  def create_or_find_protocol_subscription(the_protocol, the_start_date, the_mentor)
+  def create_or_find_protocol_subscription(the_protocol, the_start_date)
     prot_sub = nil
     prot_sub = person.protocol_subscriptions.active.where(protocol_id: the_protocol.id).first if only_if_not_subscribed
     prot_sub || ProtocolSubscription.create!(
       protocol: the_protocol,
       person: person,
-      filling_out_for: the_mentor,
+      filling_out_for: mentor,
       state: ProtocolSubscription::ACTIVE_STATE,
       start_date: the_start_date,
       end_date: end_date
     )
   end
-  # rubocop:enable Metrics/AbcSize
 
   def find_start_date
     # Active interaction weirdness. For some reason if we do not first copy

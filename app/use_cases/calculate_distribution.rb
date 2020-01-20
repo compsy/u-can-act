55385bcf19b0a07f1150f33e06d989eb981fea89
Ando Emerencia <ando.emerencia@gmail.com>
2020-01-20 16:13:52 +0100

    also add support for likert questions

--- app/use_cases/calculate_distribution.rb
+++ app/use_cases/calculate_distribution.rb
@@ -42,7 +42,7 @@ class CalculateDistribution < ActiveInteraction::Base
 
   def other_questions(questionnaire_content)
     questionnaire_content
-      .select { |question| %i[number radio].include?(question[:type]) }
+      .select { |question| %i[number radio likert].include?(question[:type]) }
       .map do |question|
       { id: question[:id].to_s, type: question[:type] }
     end

298d2ea9578ab5f52c542b5a309266fe6ad21132
Ando Emerencia <ando.emerencia@gmail.com>
2020-01-24 14:36:30 +0100

    made realtime statistics optional

--- app/models/response.rb
+++ app/models/response.rb
@@ -113,7 +113,9 @@ class Response < ApplicationRecord
     update!(completed_at: Time.zone.now,
             filled_out_by: protocol_subscription.person,
             filled_out_for: protocol_subscription.filling_out_for)
-    CalculateDistributionJob.perform_later(id)
+    if Rails.application.config.settings.feature_toggles.realtime_distributions
+      CalculateDistributionJob.perform_later(id)
+    end
     return unless first_complete && protocol_subscription.protocol.push_subscriptions.present?
 
     PushSubscriptionsJob.perform_later(self)

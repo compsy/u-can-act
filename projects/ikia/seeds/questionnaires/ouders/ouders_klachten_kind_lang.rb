0e5fccbe49b499d7fbf2ecdeb4a8f4740ad6c280
AnneMargit <44840607+AnneMargit@users.noreply.github.com>
2020-03-09 15:32:53 +0100

    Update ouders_klachten_kind_lang.rb

--- projects/ikia/seeds/questionnaires/ouders/ouders_klachten_kind_lang.rb
+++ projects/ikia/seeds/questionnaires/ouders/ouders_klachten_kind_lang.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-db_title = 'Klachten van mijn kind'
+db_title = 'Klachten (lang)'
 db_name1 = 'Klachten_Kinderen_Lang_Ouderrapportage_6plus'
 dagboek1 = Questionnaire.find_by_key(File.basename(__FILE__)[0...-3])
 dagboek1 ||= Questionnaire.new(key: File.basename(__FILE__)[0...-3])

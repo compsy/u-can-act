79a835b6ae74f84dd0962eb3f81e35e98749e9f0
AnneMargit <44840607+AnneMargit@users.noreply.github.com>
2020-03-09 15:44:07 +0100

    Update ouders_welbevinden_kind.rb

--- projects/ikia/seeds/questionnaires/ouders/ouders_welbevinden_kind.rb
+++ projects/ikia/seeds/questionnaires/ouders/ouders_welbevinden_kind.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-db_title = 'Leefplezier van mijn kind'
+db_title = 'Leefplezier'
 db_name1 = 'Welbevinden_Kind_Ouderrapportage_6plus'
 dagboek1 = Questionnaire.find_by_key(File.basename(__FILE__)[0...-3])
 dagboek1 ||= Questionnaire.new(key: File.basename(__FILE__)[0...-3])

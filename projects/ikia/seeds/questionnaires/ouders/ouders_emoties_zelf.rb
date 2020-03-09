bd2465d1746d3a3337af643c94c82e480d270e6b
AnneMargit <44840607+AnneMargit@users.noreply.github.com>
2020-03-09 15:20:02 +0100

    Update ouders_emoties_zelf.rb

--- projects/ikia/seeds/questionnaires/ouders/ouders_emoties_zelf.rb
+++ projects/ikia/seeds/questionnaires/ouders/ouders_emoties_zelf.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-db_title = 'Mijn gevoelens'
+db_title = 'Gevoelens'
 
 db_name1 = 'Emoties_Ouders_Zelf'
 dagboek1 = Questionnaire.find_by_key(File.basename(__FILE__)[0...-3])

d95d17d953fe627c1a4617617236fd959bf77e68
Ando Emerencia <ando.emerencia@gmail.com>
2020-03-11 16:44:17 +0100

    add questionnaire create api route

--- config/routes.rb
+++ config/routes.rb
@@ -132,6 +132,8 @@ Rails.application.routes.draw do
             post :change_to_mentor
           end
         end
+
+        resources :questionnaires, only: [:create]
       end
 
       # Admin APIs

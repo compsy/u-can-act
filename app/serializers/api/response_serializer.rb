3d3bb351f71cbdc7a854b8b0406170d895d1cec9
Ando Emerencia <ando.emerencia@gmail.com>
2020-03-31 00:23:08 +0200

    add expires_at attribute

--- app/serializers/api/response_serializer.rb
+++ app/serializers/api/response_serializer.rb
@@ -3,7 +3,7 @@
 module Api
   class ResponseSerializer < ActiveModel::Serializer
     type 'responses'
-    attributes %i[uuid open_from opened_at completed_at values]
+    attributes %i[uuid open_from expires_at opened_at completed_at values]
     has_one :questionnaire do
       QuestionnaireShortSerializer.new(object.measurement.questionnaire)
     end

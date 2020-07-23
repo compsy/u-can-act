f653a6854e99773dde3151abf9d3bd8b5a344b04
Ando Emerencia <ando.emerencia@gmail.com>
2020-07-23 02:13:45 +0200

    pass the group id on to the pushed response to the base platform

--- app/serializers/api/response_serializer.rb
+++ app/serializers/api/response_serializer.rb
@@ -3,9 +3,13 @@
 module Api
   class ResponseSerializer < ActiveModel::Serializer
     type 'responses'
-    attributes %i[uuid open_from expires_at opened_at completed_at values]
+    attributes %i[uuid open_from expires_at opened_at completed_at values external_identifier]
     has_one :questionnaire do
       QuestionnaireShortSerializer.new(object.measurement.questionnaire)
+    end
+
+    def external_identifier
+      object.protocol_subscription.external_identifier # can be blank
     end
   end
 end

aa9a28b6aa111481142f04143a97116939f615d7
Rik <rik@huijzer.xyz>
2020-08-04 21:24:52 +0200

    Set sane protocol duration

--- projects/ysf/seeds/protocols/kct.rb
+++ projects/ysf/seeds/protocols/kct.rb
@@ -1,5 +1,5 @@
-default_protocol_duration = 30.years  # evt eerder dynamisch afbreken
-default_open_duration = 30.years
+default_protocol_duration = 5.months
+default_open_duration = 5.months
 default_posttest_open_duration = nil
 default_reward_points = 100
 
@@ -54,8 +54,8 @@ questionnaires.each do |name|
   raise "Cannot find questionnaire: #{name}" unless dagboekvragenlijst_id
   db_measurement = protocol.measurements.where(questionnaire_id: dagboekvragenlijst_id).first
   db_measurement ||= protocol.measurements.build(questionnaire_id: dagboekvragenlijst_id)
-  # Repeat after one day.
-  db_measurement.period = 1.days
+  # Repeat after a few days, that way it should be available again in time.
+  db_measurement.period = 5.days
   db_measurement.open_duration = default_open_duration
   db_measurement.open_from_offset = 0
   db_measurement.reward_points = default_reward_points

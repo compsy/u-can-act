c3368ddf80c414698c8b85e57a82954ab97f338f
Rik <rik@huijzer.xyz>
2020-08-04 11:30:34 +0200

    Add maandag and vrijdag

--- projects/ysf/seeds/protocols/kct.rb
+++ projects/ysf/seeds/protocols/kct.rb
@@ -3,47 +3,59 @@ default_open_duration = 30.years
 default_posttest_open_duration = nil
 default_reward_points = 100
 
-###############
-## kct ##
-###############
-
 pr_name = 'kct'
 protocol = Protocol.find_by(name: pr_name)
 protocol ||= Protocol.new(name: pr_name)
 protocol.duration = default_protocol_duration
-protocol.informed_consent_questionnaire = nil
+protocol.informed_consent_questionnaire = 'KCT Alfa'
 protocol.save!
 
-# Add dagboekmetingen
 questionnaires = [
   'KCT Alfa',
   'KCT Bravo',
   'KCT Charlie',
   'KCT Delta',
-  'KCT Delta VCO',
-  'KCT Delta Compagnie',
   'KCT Echo',
-  'KCT Echo VCO',
-  'KCT Echo Compagnie',
   'KCT Foxtrot',
   'KCT Golf',
   'KCT Hotel',
-  'KCT Hotel VCO',
-  'KCT Hotel Compagnie',
   'KCT India',
   'KCT Julliet',
   'KCT Kilo',
   'KCT Lima',
-  'KCT Mike',
+  'KCT Mike'
 ]
 
 questionnaires.each do |name|
-  of_offset = 0.days # Thursday noon
+  of_offset = 0.days
   dagboekvragenlijst_id = Questionnaire.find_by(name: name)&.id
   raise "Cannot find questionnaire: #{name}" unless dagboekvragenlijst_id
   db_measurement = protocol.measurements.where(questionnaire_id: dagboekvragenlijst_id).first
   db_measurement ||= protocol.measurements.build(questionnaire_id: dagboekvragenlijst_id)
   db_measurement.period = nil
+  db_measurement.open_duration = default_open_duration
+  db_measurement.open_from_offset = 0
+  db_measurement.reward_points = default_reward_points
+  db_measurement.stop_measurement = false
+  db_measurement.should_invite = true
+  db_measurement.save!
+end
+
+questionnaires = [
+  'KCT Maandag',
+  'KCT Vrijdag'
+]
+
+# It is unkown when exactly the KCT wants to show `KCT Maandag` or `KCT Vrijdag`, so the
+# questionnaires are set to be available again after one day and are hidden by the frontend.
+questionnaires.each do |name|
+  of_offset = 0.days
+  dagboekvragenlijst_id = Questionnaire.find_by(name: name)&.id
+  raise "Cannot find questionnaire: #{name}" unless dagboekvragenlijst_id
+  db_measurement = protocol.measurements.where(questionnaire_id: dagboekvragenlijst_id).first
+  db_measurement ||= protocol.measurements.build(questionnaire_id: dagboekvragenlijst_id)
+  # Repeat after one day.
+  db_measurement.period = 1.days
   db_measurement.open_duration = default_open_duration
   db_measurement.open_from_offset = 0
   db_measurement.reward_points = default_reward_points

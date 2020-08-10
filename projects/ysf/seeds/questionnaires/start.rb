861f25740d348e5b84bbdb33df466cbf213ce0bc
Rik <rik@huijzer.xyz>
2020-08-10 10:11:44 +0200

    Refactor start.rb

--- projects/ysf/seeds/questionnaires/start.rb
+++ projects/ysf/seeds/questionnaires/start.rb
@@ -13,13 +13,7 @@ def create_injury_question(id, title)
     type: :radio,
     title: title,
     show_otherwise: false,
-    options: [
-      '1 (geen last)',
-      '2',
-      '3 (een beetje last)',
-      '4',
-      '5 (heel veel last)'
-    ]
+    options: %w[1 2 3]
   }
   res
 end
@@ -37,6 +31,8 @@ content = [
     <p class="flow-text section-explanation">
     Hieronder vind je een lijst met uitdrukkingen die verschillende aspecten van je huidige staat van herstel en stresstoestand beschrijven.
     Deze vragenlijst wordt gebruikt als hulpmiddel om jou en de gehele groep zo goed mogelijk te kunnen begeleiden tijdens de opleiding.
+
+    De vragen lopen van <b>0 (helemaal niet van toepassing)</b> tot <b>6 (volledig van toepassing)</b>.
     '
   }, {
     section_start: 'Deel 1: Beoordeel hoe jij je <b>nu voelt</b> in relatie tot je beste herstel-status ooit.',
@@ -44,57 +40,25 @@ content = [
     type: :radio,
     required: true,
     title: '<b>Physical Performance Capability</b>, bijv. fysiek sterk; fit; energiek; vol kracht.',
-    options: [
-      '0 (helemaal niet van toepassing)',
-      '1',
-      '2',
-      '3 (neutraal)',
-      '4',
-      '5',
-      '6 (volledig van toepassing)',
-    ],
+    options: %w[0 1 2 3 4 5 6],
     show_otherwise: false,
   }, {
     id: :v2,
     type: :radio,
     title: '<b>Mental Performance Capability</b>, bijv. attent/oplettend; ontvankelijk, gevoelig; geconcentreerd; alert.',
-    options: [
-      '0 (helemaal niet van toepassing)',
-      '1',
-      '2',
-      '3 (neutraal)',
-      '4',
-      '5',
-      '6 (volledig van toepassing)',
-    ],
+    options: %w[0 1 2 3 4 5 6],
     show_otherwise: false,
   }, {
     id: :v3,
     type: :radio,
     title: '<b>Emotional Balance</b>, bijv. tevreden; evenwichtig; in een goede bui; alles onder controle hebben; stabiel; verheugd.',
-    options: [
-      '0 (helemaal niet van toepassing)',
-      '1',
-      '2',
-      '3 (neutraal)',
-      '4',
-      '5',
-      '6 (volledig van toepassing)',
-    ],
+    options: %w[0 1 2 3 4 5 6],
     show_otherwise: false,
   }, {
     id: :v4,
     type: :radio,
     title: '<b>Overall Recovery (herstel)</b>, bijv. teruggewonnen; uitgerust; spierontspanning; fysiek ontspannen.',
-    options: [
-      '0 (helemaal niet van toepassing)',
-      '1',
-      '2',
-      '3 (neutraal)',
-      '4',
-      '5',
-      '6 (volledig van toepassing)',
-    ],
+    options: %w[0 1 2 3 4 5 6],
     show_otherwise: false,
     section_end: true
   }, {
@@ -102,66 +66,52 @@ content = [
     id: :v5,
     type: :radio,
     title: '<b>Muscular Stress</b>, bijv. stijve spieren; spierpijn; vermoeide spieren.',
-    options: [
-      '0 (helemaal niet van toepassing)',
-      '1',
-      '2',
-      '3 (neutraal)',
-      '4',
-      '5',
-      '6 (volledig van toepassing)',
-    ],
+    options: %w[0 1 2 3 4 5 6],
     show_otherwise: false,
   }, {
     id: :v6,
     type: :radio,
     title: '<b>Lack of Activation</b>, bijv. ongemotiveerd; ongeïnteresseerd; geen fut; loom / weinig energie.',
-    options: [
-      '0 (helemaal niet van toepassing)',
-      '1',
-      '2',
-      '3 (neutraal)',
-      '4',
-      '5',
-      '6 (volledig van toepassing)',
-    ],
+    options: %w[0 1 2 3 4 5 6],
     show_otherwise: false,
   }, {
     id: :v7,
     type: :radio,
     title: '<b>Negative Emotional State</b>, bijv. neerslachtig; gestressed; geërgerd; kort lontje; verdrietig.',
-    options: [
-      '0 (helemaal niet van toepassing)',
-      '1',
-      '2',
-      '3 (neutraal)',
-      '4',
-      '5',
-      '6 (volledig van toepassing)',
-    ],
+    options: %w[0 1 2 3 4 5 6],
     show_otherwise: false,
   }, {
     id: :v8,
     type: :radio,
     title: '<b>Overall Stress</b>, bijv. moe; uitgeput; overbelast; fysiek afgemat.',
-    options: [
-      '0 (helemaal niet van toepassing)',
-      '1',
-      '2',
-      '3 (neutraal)',
-      '4',
-      '5',
-      '6 (volledig van toepassing)',
-    ],
+    options: %w[0 1 2 3 4 5 6],
     show_otherwise: false,
     section_end: true,
   }, {
     type: :raw,
     content: '
     <p class="flow-text section-explanation">
-    Geef, met behulp van onderstaande afbeelding, zo nauwkeurig mogelijk de plaats(en) op het lichaam aan waar je de afgelopen dagen klachten had en hoeveel last je had.
+      Geef, met behulp van onderstaande afbeelding, zo nauwkeurig mogelijk de plaats(en) op het lichaam aan waar je de afgelopen dagen klachten had en hoeveel last je had.
 
-    <p class=\"flow-text\">#{title}</p><img src=\"/images/questionnaires/kct/blessures.jpg\" style=\"width: 80%; margin-left: 3rem;\" />
+      <img src="/images/questionnaires/kct/blessures.jpg" style="width: 80%; margin-left: 3rem;" />
+    </p>
+
+    <p class="flow-text section-explanation">
+      De betekenis van de cijfers 1 tot en met 5 is als volgt:
+
+      <ul class="flow-text section-explanation">
+        <li>
+        1 = geen last
+        </li>
+
+        <li>
+        2 = een beetje last
+        </li>
+
+        <li>
+        3 = heel veel last
+        </li>
+      </ul>
     </p>
     '
   },

8025e020028dc5df88dba992ed607431074a8483
Ando Emerencia <ando.emerencia@gmail.com>
2020-09-04 03:52:20 +0200

    fix specs

--- app/serializers/api/person_serializer.rb
+++ app/serializers/api/person_serializer.rb
@@ -3,7 +3,8 @@
 module Api
   class PersonSerializer < ActiveModel::Serializer
     type 'people'
-    attributes :first_name, :last_name, :gender, :email, :mobile_phone, :iban, :id, :account_active, :my_open_responses, :auth0_id_string
+    attributes :first_name, :last_name, :gender, :email, :mobile_phone, :iban, :id,
+               :account_active, :my_open_responses, :auth0_id_string
 
     def my_open_responses
       object.my_open_responses(false).map { |response| ResponseSerializer.new(response) }

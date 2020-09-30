548ce54b23110131d5476cdb46818637f4031a08
Ando Emerencia <ando.emerencia@gmail.com>
2020-09-30 16:19:21 +0200

    fix double questionnaire response bug

--- app/jobs/rescheduling_job.rb
+++ app/jobs/rescheduling_job.rb
@@ -5,7 +5,14 @@ class ReschedulingJob < ApplicationJob
 
   def perform
     ProtocolSubscription.active.find_each do |protocol_subscription|
-      RescheduleResponses.run!(protocol_subscription: protocol_subscription)
+      # Since we are rescheduling responses and not creating the responses for the first time,
+      # we want a `future` date that is actually in the future, so that we don't accidentally create
+      # responses that the user has already filled out. The extra added hour here is to ensure that
+      # if there were invitations being sent out by the worker process right now, or are in queue
+      # to be sent out, that we leave those unaffected. Again, because the responses have already
+      # been scheduled, this should be fine.
+      RescheduleResponses.run!(protocol_subscription: protocol_subscription,
+                               future: TimeTools.increase_by_duration(Time.zone.now, 1.hour))
     end
   end
 

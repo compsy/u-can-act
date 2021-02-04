1b1df294b9efef8b8644e32586103b49185370cd
Ando Emerencia <ando.emerencia@gmail.com>
2021-02-04 15:14:41 +0100

    refactoring and adding comments

--- app/controllers/token_authentication_controller.rb
+++ app/controllers/token_authentication_controller.rb
@@ -3,7 +3,8 @@
 class TokenAuthenticationController < ApplicationController
   before_action :check_params
   before_action :check_invitation_token
-  before_action :set_all_responses
+  before_action :set_attached_responses
+  before_action :set_response_to_redirect_to
 
   RESPONSE_ID_COOKIE = :response_id
   JWT_TOKEN_COOKIE = :jwt_token
@@ -11,31 +12,49 @@ class TokenAuthenticationController < ApplicationController
 
   def show
     current_person = @attached_responses.first.person
-    only_non_completed_responses = InvitationToken.find_attached_responses(questionnaire_params[:q], true)
-    if current_person.mentor? &&
-       (!@attached_responses.first.protocol_subscription.for_myself? || only_non_completed_responses.blank?)
-      # If we are a mentor, and the first response was already filled out or there is no response to fill out,
-      # redirect to the mentor dashboard and/or follow the logic of the next page finder.
+    if @response_to_redirect_to.blank?
+      # If all responses for this invitation set were filled out,
+      if current_person.mentor?
+        # then a mentor can still use the link
+        # from their invitation to view the mentor dashboard. Note that the link is only valid as long
+        # as the InvitationToken is valid (and expired Invitation tokens by default expire after 7 days).
+        # So this guarantees that any mentor can use any link sent to them for 7 days to get to their
+        # mentor dashboard, even if they filled out all responses.
+        redirect_to questionnaire_index_path
+      else
+        # A regular user however has no dashboard to go to, so if there are no more responses to fill out,
+        # their invitation link will render a 404 error.
+        render(status: :not_found, html: 'Deze link is niet (meer) geldig.', layout: 'application')
+      end
+      return
+    end
+    # We know now that we have at least one response to fill out. We prefer to redirect to the first one
+    # of this set (note that these responses are ordered by the {Response#priority_sorting_metric}).
+    # If we are a mentor and the first response to be filled out is for a student, then redirect to the
+    # mentor dashboard instead.
+    if current_person.mentor? && !@response_to_redirect_to.protocol_subscription.for_myself?
       redirect_to questionnaire_index_path
-    elsif only_non_completed_responses.present?
-      # If we are not a mentor or the prot sub is for ourselves, then we want to redirect to the response,
-      # but only if it is not yet completed. Otherwise give an error.
-      # If we have an open response to redirect to, give priority to the response from
-      # the set we actually clicked on (i.e., the first in the invitation set that is still open).
-      redirect_to preference_questionnaire_index_path(uuid: only_non_completed_responses.first.uuid)
     else
-      # If there are no responses to be filled out, and we are not a mentor, render an error.
-      render(status: :not_found, html: 'Deze link is niet (meer) geldig.', layout: 'application')
+      # If we are not a mentor or the response to be filled out is for ourselves,
+      # redirect to the response.
+      redirect_to preference_questionnaire_index_path(uuid: @response_to_redirect_to.uuid)
     end
   end
 
   private
 
-  def set_all_responses
+  def set_attached_responses
     @attached_responses = InvitationToken.find_attached_responses(questionnaire_params[:q], false)
     return if @attached_responses.present?
 
     render(status: :not_found, html: 'Deze link is niet (meer) geldig.', layout: 'application')
+  end
+
+  def set_response_to_redirect_to
+    @response_to_redirect_to = InvitationToken.find_attached_responses(questionnaire_params[:q], true).first
+
+    # I think if you return false or nil from a before_action, it stops the rendering?
+    true
   end
 
   def check_invitation_token

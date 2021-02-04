17f44fe09c329f56d1451a651c4bdf30937fcfcc
Ando Emerencia <ando.emerencia@gmail.com>
2021-02-04 14:49:37 +0100

    refactoring

--- app/controllers/token_authentication_controller.rb
+++ app/controllers/token_authentication_controller.rb
@@ -10,29 +10,30 @@ class TokenAuthenticationController < ApplicationController
   PERSON_ID_COOKIE = :person_id
 
   def show
-    current_person = @all_responses.first.person
+    current_person = @attached_responses.first.person
     only_non_completed_responses = InvitationToken.find_attached_responses(questionnaire_params[:q], true)
     if current_person.mentor? &&
-       (!@all_responses.first.protocol_subscription.for_myself? || only_non_completed_responses.blank?)
+       (!@attached_responses.first.protocol_subscription.for_myself? || only_non_completed_responses.blank?)
       # If we are a mentor, and the first response was already filled out or there is no response to fill out,
       # redirect to the mentor dashboard and/or follow the logic of the next page finder.
       redirect_to questionnaire_index_path
-    elsif only_non_completed_responses.blank?
+    elsif only_non_completed_responses.present?
       # If we are not a mentor or the prot sub is for ourselves, then we want to redirect to the response,
       # but only if it is not yet completed. Otherwise give an error.
-      render(status: :not_found, html: 'Deze link is niet (meer) geldig.', layout: 'application')
-    else
       # If we have an open response to redirect to, give priority to the response from
       # the set we actually clicked on (i.e., the first in the invitation set that is still open).
       redirect_to preference_questionnaire_index_path(uuid: only_non_completed_responses.first.uuid)
+    else
+      # If there are no responses to be filled out, and we are not a mentor, render an error.
+      render(status: :not_found, html: 'Deze link is niet (meer) geldig.', layout: 'application')
     end
   end
 
   private
 
   def set_all_responses
-    @all_responses = InvitationToken.find_attached_responses(questionnaire_params[:q], false)
-    return if @all_responses.present?
+    @attached_responses = InvitationToken.find_attached_responses(questionnaire_params[:q], false)
+    return if @attached_responses.present?
 
     render(status: :not_found, html: 'Deze link is niet (meer) geldig.', layout: 'application')
   end

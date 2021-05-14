b91ab895f23e7484a5fffdb5f5d3bcc880352a24
Ando Emerencia <a.c.emerencia@researchable.nl>
2021-05-14 18:09:11 +0200

    build(yarn): update webpack

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-const environment = require('./environment')
+const webpackConfig = require('./base')
 
 // Re-generate manifest file when not in test env
 // https://github.com/danethurber/webpack-manifest-plugin
@@ -10,4 +10,4 @@ environment.plugins.get('Manifest').opts.writeToFileEmit = process.env.NODE_ENV 
 // https://webpack.js.org/configuration/devtool/
 environment.config.set('devtool', 'inline-source-map')
 
-module.exports = environment.toWebpackConfig()
+module.exports = webpackConfig

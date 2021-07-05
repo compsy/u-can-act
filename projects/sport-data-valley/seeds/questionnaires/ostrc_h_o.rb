11b1d89ff17e3148c1e280b0fd59bdd83201a0ae
Ando Emerencia <a.c.emerencia@researchable.nl>
2021-07-05 18:02:12 +0200

    feat(sdv): remove ostrc_h and add ostrc_h_o questionnaires

--- projects/sport-data-valley/seeds/questionnaires/ostrc_h_o.rb
+++ projects/sport-data-valley/seeds/questionnaires/ostrc_h_o.rb
@@ -2,10 +2,12 @@
 
 db_title = ''
 
-db_name1 = 'ostrc_h'
+db_name1 = 'ostrc_h_o'
 questionnaire = Questionnaire.find_by(name: db_name1)
 questionnaire ||= Questionnaire.new(name: db_name1)
 questionnaire.key = File.basename(__FILE__)[0...-3]
+
+require File.expand_path('../questionnaire_helpers/complaints.rb', __dir__)
 
 class PrefixMethods
   class << self
@@ -20,17 +22,22 @@ class PrefixMethods
 end
 
 shown_questions_health = PrefixMethods::prefix_all(2, 3, 4, 5, 6, 7)
+shown_questions_injury = PrefixMethods::prefix_all('_o_1', '_o_3')
 
 dagboek_content = [
   {
+    type: :raw,
+    content: '<h4 class="header">OSTRC H+O</h4><p class="flow-text">De volgende vragen gaan over eventuele hinder die je hebt ondervonden bij het sporten fysieke en/of mentale klachten. Kies bij elke vraag het antwoord wat jouw situatie het beste omschrijft. Kies bij twijfel het meest passende antwoord.</p>'
+  },
+  {
     id: :v1,
     type: :radio,
-    title: 'In hoeverre heb je de afgelopen 7 dagen tijdens je trainingen en/of wedstrijden hinder ondervonden van gezondheidsklachten?',
+    title: 'Heb je in de afgelopen 7 dagen hinder ondervonden bij het sporten ten gevolge van een blessure, ziekte of andere gezondheidsproblemen?',
     options: [
-      { title: 'Ik heb geen gezondheidsklachten gehad', numeric_value: 0 },
-      { title: 'Ik heb volledig deelgenomen maar had wel hinder van gezondheidsklachten', numeric_value: 8, shows_questions: shown_questions_health },
-      { title: 'Ik heb verminderd deelgenomen vanwege gezondheidsklachten', numeric_value: 17, shows_questions: shown_questions_health },
-      { title: 'Ik heb helemaal niet deelgenomen vanwege gezondheidsklachten', numeric_value: 100 }
+      { title: 'Nee, ik heb volledig deelgenomen zonder klachten', numeric_value: 0 },
+      { title: 'Ja, ik heb hinder ondervonden door <strong>een blessure / lichamelijke klachten</strong>', numeric_value: 8, shows_questions: shown_questions_injury },
+      { title: 'Ja, ik heb hinder ondervonden door <strong>een ziekte / gezondheidsklachten</strong>', numeric_value: 17, shows_questions: shown_questions_health },
+      { title: 'Ja, ik heb hinder ondervonden door <strong>beide</strong>, zowel een blessure / lichamelijke klachten als een ziekte / gezondheidsklachten', numeric_value: 100, shows_questions: shown_questions_health + shown_questions_injury }
     ],
     show_otherwise: false
   },
@@ -125,8 +132,28 @@ dagboek_content = [
     type: :radio,
     title: 'Ben je voor je gezondheidsklachten de afgelopen 7 dagen behandeld door een therapeut of arts?',
     options: %w[Ja Nee],
-    show_otherwise: false
-  }
+    show_otherwise: false,
+  },
+  {
+    id: :v_o_1,
+    hidden: true,
+    type: :number,
+    title: 'Hoeveel uur heb je de afgelopen week gesport?',
+    required: true,
+    maxlength: 3,
+    min: 0,
+    max: 100
+  },
+  {
+    id: :v_o_3,
+    hidden: true,
+    type: :checkbox,
+    title: 'In welke regio vond(en) de klacht(en) plaats?',
+    options: Complaints::all_complaint_options,
+    show_otherwise: false,
+    required: true
+  },
+  *Complaints::all_complaint_questions
 ]
 
 questionnaire.content = {
--- projects/sport-data-valley/seeds/seeds.rb
+++ projects/sport-data-valley/seeds/seeds.rb
@@ -81,11 +81,11 @@ if Person.all.select{|person| person.auth_user.blank?}.count == 0 && (Rails.env.
   puts ''
   protocol = Protocol.find_by(name: 'squash_otr')
   token = 'squash'
-  OneTimeResponse.create!(token: token, protocol: protocol)
+  OneTimeResponse.create!(token: token, protocol: protocol) unless OneTimeResponse.exists?(token: token)
   puts "One time response: #{Rails.application.routes.url_helpers.one_time_response_url(q: token)}"
 
   # Create ostrc_o_h protocol instance
-  protocol = Protocol.find_by(name: 'ostrc_o_h')
+  protocol = Protocol.find_by(name: 'ostrc_h_o')
   person = Team.find_by_name(demo_team).roles.where(group: Person::STUDENT).first.people[1]
   prot_sub = ProtocolSubscription.create!(
     protocol: protocol,
@@ -99,6 +99,6 @@ if Person.all.select{|person| person.auth_user.blank?}.count == 0 && (Rails.env.
   prot_sub.responses.first.update_attributes!(open_from: 1.minute.ago, invitation_set: invitation_set)
 
   invitation_token = invitation_set.invitation_tokens.create!
-  puts "OSTRC O+H protocol: #{invitation_set.invitation_url(invitation_token.token_plain)}"
+  puts "OSTRC H+O protocol: #{invitation_set.invitation_url(invitation_token.token_plain)}"
 
 end

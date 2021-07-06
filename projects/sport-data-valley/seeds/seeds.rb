337f3f0ec7100566d209d47ce4c1775bbefa7ad4
Ando Emerencia <a.c.emerencia@researchable.nl>
2021-07-06 21:01:50 +0200

    feat(gentle): remove demo questionnaires

--- projects/sport-data-valley/seeds/seeds.rb
+++ projects/sport-data-valley/seeds/seeds.rb
@@ -81,7 +81,7 @@ if Person.all.select{|person| person.auth_user.blank?}.count == 0 && (Rails.env.
   puts ''
   protocol = Protocol.find_by(name: 'squash_otr')
   token = 'squash'
-  OneTimeResponse.create!(token: token, protocol: protocol) unless OneTimeResponse.exists?(token: token)
+  OneTimeResponse.create!(token: token, protocol: protocol)
   puts "One time response: #{Rails.application.routes.url_helpers.one_time_response_url(q: token)}"
 
   # Create ostrc_o_h protocol instance

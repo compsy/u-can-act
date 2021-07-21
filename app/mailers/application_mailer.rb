28fa6f6d99264f16037a42f81d3f0b1f2ce66c3e
Ando Emerencia <a.c.emerencia@researchable.nl>
2021-07-21 20:28:24 +0200

    docs(email): clarify comment

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,8 +3,9 @@
 class ApplicationMailer < ActionMailer::Base
   helper ApplicationHelper
 
-  # Make it so that we can also find non-default layouts that exist
-  # in the project directories
+  # If a template or layout exists in the project directory, prefer it over the default.
+  # This works fine even when the views subdirectory does not exist.
+  # (Rails won't complain. It is used to having lots of paths in its search space that don't exist.)
   prepend_view_path(Rails.root.join('projects', ENV['PROJECT_NAME'], 'views'))
 
   default from: 'from@example.com'

c9662ed555f259f2a42ffadbd1c76aa267ae9cb7
Ando Emerencia <a.c.emerencia@researchable.nl>
2021-07-27 19:17:54 +0200

    test(specs): add specs for external_identifiers

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -357,6 +357,38 @@ describe Response do
     end
   end
 
+  describe 'external_identifiers', focus: true do
+    it 'works when there are clones' do
+      prot = FactoryBot.create(:protocol, :with_measurements)
+      protsub1 = FactoryBot.create(:protocol_subscription, protocol: prot, external_identifier: 'my-ext-1')
+      protsub2 = FactoryBot.create(:protocol_subscription, person: protsub1.person, protocol: prot, external_identifier: 'my-ext-2')
+      responseobj = FactoryBot.create(:response, :completed, protocol_subscription: protsub1, measurement: prot.measurements[0])
+      responseobj2 = FactoryBot.create(:response, :completed, protocol_subscription: protsub2, measurement: prot.measurements[0])
+      expect(responseobj.external_identifiers).to match_array(%w[my-ext-1 my-ext-2])
+      expect(responseobj2.external_identifiers).to match_array(%w[my-ext-1 my-ext-2])
+    end
+
+    it 'does not work when person is different' do
+      prot = FactoryBot.create(:protocol, :with_measurements)
+      protsub1 = FactoryBot.create(:protocol_subscription, protocol: prot, external_identifier: 'my-ext-1')
+      protsub2 = FactoryBot.create(:protocol_subscription, protocol: prot, external_identifier: 'my-ext-2')
+      responseobj = FactoryBot.create(:response, :completed, protocol_subscription: protsub1, measurement: prot.measurements[0])
+      responseobj2 = FactoryBot.create(:response, :completed, protocol_subscription: protsub2, measurement: prot.measurements[0])
+      expect(responseobj.external_identifiers).to match_array(%w[my-ext-1])
+      expect(responseobj2.external_identifiers).to match_array(%w[my-ext-2])
+    end
+
+    it 'does not work when dates are different is different' do
+      prot = FactoryBot.create(:protocol, :with_measurements)
+      protsub1 = FactoryBot.create(:protocol_subscription, protocol: prot, external_identifier: 'my-ext-1')
+      protsub2 = FactoryBot.create(:protocol_subscription, person: protsub1.person, protocol: prot, external_identifier: 'my-ext-2')
+      responseobj = FactoryBot.create(:response, :completed, open_from: Time.zone.now, protocol_subscription: protsub1, measurement: prot.measurements[0])
+      responseobj2 = FactoryBot.create(:response, :completed, protocol_subscription: protsub2, measurement: prot.measurements[0])
+      expect(responseobj.external_identifiers).to match_array(%w[my-ext-1])
+      expect(responseobj2.external_identifiers).to match_array(%w[my-ext-2])
+    end
+  end
+
   describe 'values' do
     it 'works when there is content' do
       responseobj = FactoryBot.create(:response, :completed)

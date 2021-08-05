08e0e9edc68790ac93ed44e0d482a84189da18e4
Ando Emerencia <a.c.emerencia@researchable.nl>
2021-08-05 16:13:48 +0200

    feat(sdv): change ostrc text and options

--- projects/sport-data-valley/seeds/questionnaires/ostrc_h_o.rb
+++ projects/sport-data-valley/seeds/questionnaires/ostrc_h_o.rb
@@ -23,19 +23,24 @@ end
 
 shown_questions_health = PrefixMethods::prefix_all(1, 5, 6, 7)
 shown_questions_injury = PrefixMethods::prefix_all('_o_2', '_o_3')
+shown_questions_hours = PrefixMethods::prefix_all('_o_3')
 shown_questions_participated = PrefixMethods::prefix_all(2, 3, 4)
 
 dagboek_content = [
   {
     type: :raw,
-    content: '<h4 class="header">OSTRC H+O</h4><p class="flow-text">De volgende vragen gaan over eventuele hinder die je hebt ondervonden bij het sporten fysieke en/of mentale klachten. Kies bij elke vraag het antwoord wat jouw situatie het beste omschrijft. Kies bij twijfel het meest passende antwoord.</p>'
+    content: '<h4 class="header">OSTRC H+O</h4><p class="flow-text">De volgende vragen gaan over eventuele hinder die je hebt ondervonden bij het sporten door lichamelijke en/of mentale klachten. Kies bij elke vraag het antwoord wat jouw situatie het beste omschrijft. Kies bij twijfel het meest passende antwoord.</p>'
   },
   {
     id: :v0,
     type: :radio,
     title: 'Heb je in de afgelopen 7 dagen hinder ondervonden bij het sporten ten gevolge van een blessure, ziekte of andere gezondheidsklachten?',
     options: [
-      { title: 'Nee, ik heb volledig deelgenomen zonder klachten', numeric_value: 0 },
+      {
+        title: 'Nee, ik heb volledig deelgenomen zonder klachten',
+        numeric_value: 0,
+        shows_questions: shown_questions_hours
+      },
       {
         title: 'Ja, ik heb hinder ondervonden door <strong>een blessure / fysieke klachten</strong>',
         numeric_value: 8,
@@ -45,7 +50,7 @@ dagboek_content = [
       {
         title: 'Ja, ik heb hinder ondervonden door <strong>een ziekte / gezondheidsklachten</strong>',
         numeric_value: 17,
-        shows_questions: shown_questions_health,
+        shows_questions: shown_questions_health + shown_questions_hours,
         tooltip: 'Let op: de klachten betreffen alleen ziekte / gezondheidsklachten. Denk hierbij aan een buikpijn, koorts, pijn op de borst etc.'
       },
       { title: 'Ja, ik heb hinder ondervonden door <strong>beide</strong>, zowel een blessure / fysieke klachten als een ziekte / gezondheidsklachten', numeric_value: 100, shows_questions: shown_questions_health + shown_questions_injury }

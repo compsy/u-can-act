bdae7c0c75530d9ab2f5a9f43a385fe67a4783e0
Ando Emerencia <a.c.emerencia@researchable.nl>
2021-09-20 18:53:51 +0200

    feat(protocol completion): remove future responses from protocol completion and add properties

--- app/serializers/api/response_serializer.rb
+++ app/serializers/api/response_serializer.rb
@@ -10,7 +10,7 @@ module Api
     # protocol subscription but from the same person and scheduled at around the same time).
     attributes %i[uuid open_from expires_at opened_at completed_at
                   values external_identifier questionnaire external_identifiers
-                  invitation_texts protocol_completion]
+                  invitation_texts protocol_completion measurement_id protocol_subscription_id]
 
     # It doesn't work with has_one because for some reason it passes the wrong object
     def questionnaire
@@ -19,9 +19,15 @@ module Api
 
     # Only calculate and send it when we specifically ask for it (e.g., for push subscriptions).
     def protocol_completion
-      return object.protocol_subscription.protocol_completion if instance_options[:with_protocol_completion]
+      return protocol_completion_without_future if instance_options[:with_protocol_completion]
 
       []
+    end
+
+    private
+
+    def protocol_completion_without_future
+      object.protocol_subscription.protocol_completion.reject { |entry| entry[:future].present? }
     end
   end
 end

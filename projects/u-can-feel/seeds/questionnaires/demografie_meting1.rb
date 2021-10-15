1b8394cff5f4be446586f477661f7b8fbfc4f805
everenn <ymkjeanna@gmail.com>
2021-10-15 10:45:06 +0200

    Update demografie_meting1.rb
    Change db_name1 to unique identifier

--- projects/u-can-feel/seeds/questionnaires/demografie_meting1.rb
+++ projects/u-can-feel/seeds/questionnaires/demografie_meting1.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 db_title = 'Start'
-db_name1 = 'demografie'
+db_name1 = 'demografie_meting1'
 dagboek1 = Questionnaire.find_by_key(File.basename(__FILE__)[0...-3])
 dagboek1 ||= Questionnaire.new(key: File.basename(__FILE__)[0...-3])
 dagboek1.name = db_name1

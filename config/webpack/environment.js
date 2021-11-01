f005e9ea957b5fde85eb8aa2a29b44e271b8c85e
Ando Emerencia <a.c.emerencia@researchable.nl>
2021-11-01 19:17:40 +0100

    fix(webpacker): fix environment variable loading

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,6 @@
 const { environment } = require('@rails/webpacker')
 const Dotenv = require('dotenv-webpack');
 
-// TODO: are these loaded by default?
-const dotenvFiles = [
-  `.env.${process.env.NODE_ENV}.local`,
-  '.env.local',
-  `.env.${process.env.NODE_ENV}`,
-  '.env'
-];
-environment.plugins.prepend('Dotenv', new Dotenv());
+environment.plugins.prepend('Dotenv', new Dotenv({ path: '.env.local', defaults: '.env' }))
 
 module.exports = environment

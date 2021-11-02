8c9cbdbd6562ffbd06b87df4a5eb043069a0a176
Ando Emerencia <a.c.emerencia@researchable.nl>
2021-11-02 09:39:15 +0100

    build(yarn): fix production build

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,6 @@
 const { environment } = require('@rails/webpacker')
 const Dotenv = require('dotenv-webpack');
 
-environment.plugins.prepend('Dotenv', new Dotenv({ path: '.env.local', defaults: '.env' }))
+environment.plugins.prepend('Dotenv', new Dotenv({ path: '.env.local', defaults: '.env', silent: true }))
 
 module.exports = environment

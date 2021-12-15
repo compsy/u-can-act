1fb06f36a536bb0fc5c676ea3b91521de560eeca
Mario Rios <m.rios.munoz@researchable.nl>
2021-12-15 11:23:53 +0100

    chore: make training response seeds random and add initial daily responses

--- bin/seed_training_responses.rb
+++ bin/seed_training_responses.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+srand(123)
 
 def find_or_create_person(email)
 
@@ -22,13 +23,12 @@ def find_or_create_person(email)
   person
 end
 
-def training_type(idx)
+def training_type
   types = %w[voetballen MTB hardlopen kracht]
-  i = idx % types.count
-  types[i]
+  types[rand(types.count)]
 end
 
-def session_type(idx)
+def session_type
   types = ['ext uithoudingsvermogen',
     'ext interval',
     'fartlek',
@@ -40,32 +40,27 @@ def session_type(idx)
     'race',
     'herstel',
   ]
-  i = idx % types.count
-  types[i]
+  types[rand(types.count)]
 end
 
-def rpe_score(idx)
+def rpe_score
   scores = (1..9).map(&:to_s)
-  i = idx % scores.count
-  scores[i]
+  scores[rand(9)]
 end
 
-def satisfaction(idx)
+def satisfaction
   scores = %w[2 2.5 3 3.5 3.5 4 4.5 5]
-  i = idx % scores.count
-  scores[i]
+  scores[rand(scores.count)]
 end
 
-def duration(idx)
+def duration
   durations = %w[30 45 60 120]
-  i = idx % durations.count
-  durations[i]
+  durations[rand(4)]
 end
 
-def time_of_start(date, idx)
+def time_of_start(date)
   offsets = [7, 14, 18, 20]
-  i = idx % offsets.count
-  date.beginning_of_day + offsets[i].hours
+  date.beginning_of_day + offsets[rand(offsets.count)].hours
 end
 
 def date_format(time_with_zone)
@@ -80,7 +75,20 @@ def minute_format(time_with_zone)
   time_with_zone.strftime('%M').to_i.to_s
 end
 
-def create_training_log_response(person, idx, timestamp)
+def get_day(date)
+  weekdays = %w[
+    maandag
+    dinsdag
+    woensdag
+    donderdag
+    vrijdag
+    zaterdag
+    zondag
+  ]
+  weekdays[date.wday]
+end
+
+def create_training_log_response(person, timestamp)
   protocol_name = 'training_log'
   puts "Creating responses for #{protocol_name}"
   protocol = Protocol.find_by(name: protocol_name)
@@ -119,17 +127,17 @@ def create_training_log_response(person, idx, timestamp)
   response_params[:completed_at] = timestamp + 1.minute
 
   content = {}
-  content[:v1] = training_type(idx)
-  content[:v2] = session_type(idx)
+  content[:v1] = training_type
+  content[:v2] = session_type
 
-  starting_at = time_of_start(timestamp, idx)
+  starting_at = time_of_start(timestamp)
   content[:v3] = date_format(starting_at)
   content[:v4_uren] = hour_format(starting_at)
   content[:v4_minuten] = minute_format(starting_at)
 
-  content[:v5] = duration(idx)
-  content[:v6] = rpe_score(idx)
-  content[:v7] = satisfaction(idx)
+  content[:v5] = duration
+  content[:v6] = rpe_score
+  content[:v7] = satisfaction
   content[:v8] = 'a comment'
 
   response_content = ResponseContent.create_with_scores!(
@@ -141,13 +149,118 @@ def create_training_log_response(person, idx, timestamp)
   PushSubscriptionsJob.perform_later(Response.create!(response_params))
 end
 
-(1..100).each do |idx|
+def create_daily_response(person, timestamp)
+  protocol_name = 'daily_protocol'
+  puts "Creating responses for #{protocol_name}"
+
+  protocol = Protocol.find_by(name: protocol_name)
+  raise "Error: protocol named #{protocol_name} not found" unless protocol
+
+  protsub = ProtocolSubscription.find_by(person: person, protocol: protocol)
+  protsub ||= ProtocolSubscription.new(person: person, protocol: protocol)
+
+  protsub.state = ProtocolSubscription::COMPLETED_STATE
+  protsub.start_date = Time.new(2016).in_time_zone
+  protsub.end_date = Time.new(2017).in_time_zone
+  protsub.save!
+
+  protsub_id = protsub.id
+  filled_out_by_id = protsub.person.id
+  filled_out_for_id = protsub.filling_out_for.id
+
+  Response.where(protocol_subscription_id: protsub_id).each do |response|
+    ResponseContent.find(response.content).destroy if response.content.present?
+    response.destroy
+  end
+
+  dag = get_day(timestamp)
+  questionnaire_name = "daily_questionnaire_#{dag}"
+  questionnaire_id = Questionnaire.find_by(name: questionnaire_name).id
+  measurement = protocol.measurements.where(questionnaire_id: questionnaire_id).first
+  measurement_id = measurement.id
+
+  response_params = {}
+  response_params[:protocol_subscription_id] = protsub_id
+  response_params[:measurement_id] = measurement_id
+  response_params[:filled_out_by_id] = filled_out_by_id
+  response_params[:filled_out_for_id] = filled_out_for_id
+
+  response_params[:open_from] = timestamp - 1.minute
+  response_params[:opened_at] = timestamp
+  response_params[:completed_at] = timestamp + 1.minute
+
+  content = {}
+  date = timestamp
+  content[:date_dontuse] = date_format(date) # store this somewhere in case we need it later
+  content[:v1] = convert_scale_value(row['Sleep quality'])
+
+  content[:v2_uren] = convert_sleep_duration_hours(row['Sleep duration (hours)'])
+  content[:v2_minuten] = convert_sleep_duration_minutes(row['Sleep duration (hours)'])
+  content[:v2_dontuse] = row['Sleep quality'].gsub(',','.')
+
+  content[:v3] = convert_scale_value(row['Fatigue'])
+  content[:v4] = convert_scale_value(row['Stress ']) # Note the space at the end of the attribute name
+  content[:v5] = convert_scale_value(row['General muscle soreness'])
+  content[:v6] = convert_scale_value(row['Mood'])
+  content[:v7] = convert_scale_value(row['Readiness-to-train'])
+  content[:v8] = row['Resting HR (bpm)']
+  content[:v9] = convert_yes_no(row['Sick?'])
+  content[:v10] = convert_yes_no(row['Injured?'])
+  content[:v11] = convert_comments(row['Comments'])
+
+  response_content = ResponseContent.create_with_scores!(
+    content: content,
+    response: Response.new(measurement_id: response_params[:measurement_id])
+  )
+  response_params[:content] = response_content.id
+
+  PushSubscriptionsJob.perform_later(Response.create!(response_params))
+
+  if dag == 'zondag'
+    questionnaire_name = "sunday_questionnaire"
+    questionnaire_id = Questionnaire.find_by(name: questionnaire_name).id
+    measurement = protocol.measurements.where(questionnaire_id: questionnaire_id).first
+    measurement_id = measurement.id
+
+    response_params = {}
+    response_params[:protocol_subscription_id] = protsub_id
+    response_params[:measurement_id] = measurement_id
+    response_params[:filled_out_by_id] = filled_out_by_id
+    response_params[:filled_out_for_id] = filled_out_for_id
+
+    timestamp = Time.zone.strptime(row['Timestamp'], '%m-%d-%Y %H:%M:%S') + TIME_OFFSET
+    response_params[:open_from] = timestamp
+    response_params[:opened_at] = timestamp
+    response_params[:completed_at] = timestamp
+    content = {}
+    date = Time.zone.strptime(row['Date'], '%m-%d-%Y %H:%M:%S') + TIME_OFFSET
+    content[:date_dontuse] = date_format(date) # store this somewhere in case we need it later
+    content[:v1] = convert_scale_value(row['Fatigue (mental)'])
+    content[:v2] = convert_scale_value(row['Fatigue (physical)'])
+    content[:v3] = convert_numerical_radio_blessures(row['Difficulties participating due to injury, illness or other health problems?'])
+    content[:v4] = convert_numerical_radio_training_volume(row['Reduced training volume due to injury, illness or other health problems?'])
+    content[:v5] = convert_numerical_radio_prestatie(row['Did injury, illness or other health problems affect your performance?'])
+    content[:v6] = convert_numerical_radio_symptoms(row['Did you experience symptoms/health complaints?'])
+    content[:v7] = convert_comments(row['How did you experience last week?'])
+
+    response_content = ResponseContent.create_with_scores!(
+      content: content,
+      response: Response.new(measurement_id: response_params[:measurement_id])
+    )
+    response_params[:content] = response_content.id
+
+    PushSubscriptionsJob.perform_later(Response.create!(response_params))
+  end
+end
+
+(1..3).each do |idx|
   email = "questionnaire_participant_#{idx}@researchable.nl"
   person = find_or_create_person(email)
 
   n_days = ((Time.zone.now - 2.months.ago)/1.day).floor
   (1..n_days).each do |days|
     date = days.days.ago.beginning_of_day + 20.hours
-    create_training_log_response(person, idx, date)
+    create_training_log_response(person, date)
+    # create_daily_response(person, date)
   end
 end

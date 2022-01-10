8a21db9ecfeeec58991c187f6be81c3843bed2a0
Ando Emerencia <a.c.emerencia@researchable.nl>
2022-01-10 18:09:53 +0100

    feat(invitations): add grace period of 6 months before cleaning up expired invitation tokens

--- app/background_tasks/cleanup_invitation_tokens.rb
+++ app/background_tasks/cleanup_invitation_tokens.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class CleanupInvitationTokens
+  # Make it so that we only clean up invitation tokens if
+  # they've been expired for 6 months. That way, if someone clicks a (less than six months old)
+  # recent link for a questionnaire that has expired, they don't get a "you are not allowed to
+  # see this questionnaire", but they'll see a "this questionnaire has expired" message instead.
+  EXPIRATION_GRACE_PERIOD = 6.months
+
   class << self
     def run
       # Since we're modifying the object, find_each would probably not work.
@@ -14,7 +20,7 @@ class CleanupInvitationTokens
 
     def cleanup_invitation_token(invitation_token)
       expires_at = invitation_token.calculate_expires_at
-      if expires_at > Time.zone.now
+      if TimeTools.increase_by_duration(expires_at, EXPIRATION_GRACE_PERIOD) > Time.zone.now
         invitation_token.expires_at = expires_at
         invitation_token.save!
         return

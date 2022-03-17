567dee64b73d21de95eb4e5d4036454e3c758c59
Mario Rios <m.rios.munoz@researchable.nl>
2022-03-17 15:19:39 +0100

    feat: add basic auth route to create a person

--- app/controllers/api/v1/basic_auth_api/person_controller.rb
+++ app/controllers/api/v1/basic_auth_api/person_controller.rb
@@ -7,6 +7,24 @@ module Api
         before_action :set_person, only: %i[change_to_mentor]
         before_action :set_mentor_role, only: %i[change_to_mentor]
 
+        def create
+          auth_user = AuthUser.from_token_payload(new_person_params)
+          # This endpoint was added for UMO, where we need to sync users and we need their real emails stored so we can
+          # invite them to fill in questionnaires. AuthUser.from_token_payload creates a new anonymous user (with no
+          # email), so we need to add it manually
+          auth_user.person.update!(email: new_person_params[Rails.application.config.settings.metadata_field][:email])
+
+          render status: :created, json: auth_user
+        rescue ActionController::ParameterMissing => e
+          # Do we have a standardized way of presenting errors?
+          render status: :bad_request, json: { error: e.message }.to_json
+        rescue StandardError => e
+          status = :internal_server_error
+          status = :bad_request if team_error?(e)
+
+          render status: status, json: { error: e.message }.to_json
+        end
+
         def show_list
           auth_users = AuthUser.where(auth0_id_string: person_params[:person_auth0_ids])
           render status: :ok, json: auth_users.map(&:person), each_serializer: Api::PersonSerializer
@@ -36,6 +54,20 @@ module Api
           return if @mentor_role
 
           render json: { error: 'Cannot assign mentor role: mentor role does not exist' }, status: :bad_request
+        end
+
+        def new_person_params
+          params.require(:person).permit :sub,
+                                         Rails.application.config.settings.metadata_field => %i[team role email]
+        end
+
+        def team_error?(e)
+          %r{
+             (Team\s\'.*\'\snot\sfound)|
+             (Team\s\'.*\'\shas\sno\sroles)|
+             (Specified\srole\s\'.*\'\snot\sfound\sin\steam)|
+             (Required\spayload\sattribute\steam\snot\sspecified)
+          }x.match?(e.message)
         end
       end
     end
--- spec/integration/api/v1/basic_auth_api/person_controller_spec.rb
+++ spec/integration/api/v1/basic_auth_api/person_controller_spec.rb
@@ -5,6 +5,69 @@ require 'swagger_helper'
 describe 'Person API' do
   let!(:people) { FactoryBot.create_list(:auth_user, 10, :with_person) }
 
+  path '/basic_auth_api/person' do
+    post 'Create a person' do
+      tags 'Person'
+      consumes 'application/json'
+      security [BasicAuth: {}]
+
+      parameter name: :person, in: :body, schema: {
+        type: :object,
+        properties: {
+          sub: { type: :string },
+          metadata: {
+            type: :object,
+            properties: {
+              team: { type: :string },
+              role: { type: :string },
+              email: { type: :string, format: :email }
+            }
+          }
+        }
+      }
+
+      let!(:Authorization) { basic_encode(ENV['API_KEY'], ENV['API_SECRET']) }
+      let!(:team) { FactoryBot.create :team, :with_roles }
+
+      let!(:person) do
+        {
+          person: {
+            sub: 'sub1',
+            Rails.application.config.settings.metadata_field => {
+              team: team.name,
+              role: team.roles.first.title,
+              email: 'email1@example.com'
+            }
+          }
+        }
+      end
+
+      response '201', 'created' do
+        run_test! do
+          auth_user = AuthUser.find_by auth0_id_string: 'sub1'
+          expect(auth_user).to be_present
+          p = Person.find_by email: 'email1@example.com'
+          expect(p).to be_present
+        end
+      end
+
+      response '400', 'bad request' do
+        let(:person) { { person: { sub: 'sub1'  } } }
+        run_test!
+      end
+
+      response '400', 'bad request', document: false do
+        let(:person) { {} }
+        run_test!
+      end
+
+      response '401', 'not authenticated' do
+        let(:Authorization) { 'Bearer nil' }
+        run_test!
+      end
+    end
+  end
+
   path '/basic_auth_api/person/show_list' do
     get 'Shows a list of persons' do
       tags 'Person'

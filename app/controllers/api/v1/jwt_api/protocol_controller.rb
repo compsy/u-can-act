d42cb71caf80462931d6163f90ed62a441c80b8e
Mario Rios <m.rios.munoz@researchable.nl>
2022-03-28 12:53:01 +0200

    Revert "refactor: move protocol creation to jwt api"
    This reverts commit 678861b564217861edf0d66aff2a83b48f575ef4.

--- app/controllers/api/v1/jwt_api/protocol_controller.rb
+++ app/controllers/api/v1/jwt_api/protocol_controller.rb
@@ -21,13 +21,6 @@ module Api
                                               preview_params[:open_from_day_uses_start_date_offset])
         end
 
-        def create
-          res = CreateOrUpdateProtocol.run **create_protocol_params
-          return created(res.protocol) if res.valid?
-
-          validation_error res.errors
-        end
-
         private
 
         def set_protocol
@@ -39,19 +32,6 @@ module Api
 
         def preview_params
           params.permit(:start_date, :end_date, :open_from_day_uses_start_date_offset)
-        end
-
-        def create_protocol_params
-          params.require(:protocol).permit(
-            [:name,
-             :duration,
-             :invitation_text,
-             :informed_consent_questionnaire_key,
-             questionnaires:
-               [:key,
-                measurement:
-                  %i[open_from_offset open_from_day period open_duration reminder_delay priority stop_measurement
-                     should_invite only_redirect_if_nothing_else_ready]]])
         end
       end
     end

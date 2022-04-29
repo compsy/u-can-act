22f5cef8dabf0ca371d913fdbdbebb453085700f
Rik Huijzer <rikhuijzer@pm.me>
2022-04-29 09:44:07 +0200

    Update questionnaire

--- projects/ysf/seeds/questionnaires/ontbijt.rb
+++ projects/ysf/seeds/questionnaires/ontbijt.rb
@@ -1,57 +1,52 @@
 # frozen_string_literal: true
 
-def create_ontbijt_question(id: Integer, titel: String)
-  v = "v".concat(id.to_s)
+ontbijt_introduction = {
+  type: :raw,
+  content: '<p class="flow-text section-explanation">
+      Paresto test in samenwerking met het KCT tijdens de VO/ECO een aantal nieuwe ontbijtvariaties zoals pannenkoeken of havermout die je naast het normale ontbijt krijgt aangeboden.
+      Deze ontbijtvariaties hebben een relatief hoge calorische waarde (±1000 Kcal) om tegemoet te komen aan het hoge energieverbruik tijdens de opleiding.
+      Het verzoek is om per ontbijttype 3 vragen te beantwoorden.
+      Alle vragen zijn optioneel dus als je een ontbijt gemist hebt dan kun je de antwoorden leeg laten.
+    </p>'
+}
+
+def create_ontbijt_question(id, titel)
+  v = "v".dup.concat(id.to_s)
   [
     {
-      id: v.concat("-textarea").to_sym,
-      type: :textarea,
-      title: "Ontbijt ".concat(id.to_s).concat(" - ").concat(titel)
-    },
-    {
-      id: v.concat("-smaak").to_sym,
-      type: :radio,
+      section_start: "Ontbijt #{id.to_s} - #{titel}",
+      id: v.dup.concat("-smaak").to_sym,
+      type: :range,
       required: false,
       title: 'Wat vind je van de smaak?',
-      options: [
-        '0' (niet lekker),
-        '1',
-        '2',
-        '3',
-        '4',
-        '5' (heel erg lekker)
+      labels: [
+        'Niet lekker',
+        'Heel erg lekker'
       ],
       show_otherwise: false
     },
     {
-      id: v.concat("-aanvulling").to_sym,
-      type: :radio,
+      id: v.dup.concat("-aanvulling").to_sym,
+      type: :range,
       required: false,
       title: 'Vind je dit ontbijttype een goede aanvulling op het "normale ontbijt" (alleen brood en beleg)?',
-      options: [
-        '0' (helemaal geen goede aanvulling),
-        '1',
-        '2',
-        '3',
-        '4',
-        '5' (een zeer goede aanvulling)
+      labels: [
+        'Helemaal geen goede aanvulling',
+        'Een zeer goede aanvulling'
       ],
       show_otherwise: false
     },
     {
-      id: v.concat("-maag").to_sym,
-      type: :radio,
+      id: v.dup.concat("-maag").to_sym,
+      type: :range,
       required: false,
       title: 'Hoe valt dit ontbijttype "op de maag" (m.n. als jij je direct na het ontbijt fysiek moet gaan inspannen)?',
-      options: [
-        '0' (zeer licht),
-        '1',
-        '2',
-        '3',
-        '4',
-        '5' (veel te zwaar)
+      labels: [
+        'Zeer licht',
+        'Veel te zwaar'
       ],
-      show_otherwise: false
+      show_otherwise: false,
+      section_end: true
     }
   ]
 end
@@ -63,6 +58,7 @@ questionnaire ||= Questionnaire.new(name: name)
 questionnaire.key = 'ontbijt'
 
 content = [
+  ontbijt_introduction,
   *create_ontbijt_question(1, "Zuivelbuffet: yoghurt en kwark, muesligranen, toegevoegde noten en honing"),
   *create_ontbijt_question(2, "Rijstepap naturel"),
   *create_ontbijt_question(3, "Rijstepap met rozijnen en banaan"),

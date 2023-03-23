7794b6a34d0d616acc068aa7c42d589c3c3d6549
Ando Emerencia <a.c.emerencia@researchable.nl>
2023-03-23 22:30:55 +0100

    Update demografie_voorjaar2023.rb

--- projects/u-can-feel/seeds/questionnaires/demografie_voorjaar2023.rb
+++ projects/u-can-feel/seeds/questionnaires/demografie_voorjaar2023.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 db_title = 'Start'
-db_name1 = 'demografie_najaar2022'
+db_name1 = 'demografie_voorjaar2023'
 dagboek1 = Questionnaire.find_by_key(File.basename(__FILE__)[0...-3])
 dagboek1 ||= Questionnaire.new(key: File.basename(__FILE__)[0...-3])
 dagboek1.name = db_name1

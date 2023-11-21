ab0360223f42b698cf98304ff4b335db0f77d6ab
ZeCarlosCoutinho <jczelik@gmail.com>
2023-11-21 16:25:12 +0100

    feat(climbing accident): create climbing accident report

---
+++ projects/sport-data-valley/seeds/protocols/climbing_accident.rb
@@ -0,0 +1,27 @@
+require_relative '../protocol_helper'
+require_relative '../questionnaires/climbing_accident'
+
+CLIMBING_ACCIDENT_PROTOCOL_NAME = 'climbing_accident'
+CLIMBING_ACCIDENT_NOTIFICATION_NAME = 'base-platform-subscription-climbing-accident'
+CLIMBING_ACCIDENT_DEFAULT_PROTOCOL_DURATION = 30.days # TODO check if this is the case
+
+protocol = create_or_update_protocol(CLIMBING_ACCIDENT_PROTOCOL_NAME, CLIMBING_ACCIDENT_DEFAULT_PROTOCOL_DURATION)
+
+# Necessary to send the filled in response to the SDV platform
+add_push_subscription(protocol, CLIMBING_ACCIDENT_NOTIFICATION_NAME)
+
+q_name = CLIMBING_ACCIDENT_QUESTIONNAIRE_NAME
+questionnaire_id = Questionnaire.find_by(name: q_name)&.id
+raise "Cannot find questionnaire: #{q_name}" unless questionnaire_id
+
+db_measurement = protocol.measurements.where(questionnaire_id: questionnaire_id).first
+db_measurement ||= protocol.measurements.build(questionnaire_id: questionnaire_id)
+db_measurement.open_from_offset = 0 # open right away
+db_measurement.period = nil # one-off and not repeated
+db_measurement.open_duration = nil # always open until the end of the protocol
+db_measurement.reward_points = 0
+db_measurement.stop_measurement = false # unsubscribe immediately
+db_measurement.should_invite = true # Send invitation
+db_measurement.redirect_url = ENV['BASE_PLATFORM_URL']
+db_measurement.only_redirect_if_nothing_else_ready = true
+db_measurement.save!

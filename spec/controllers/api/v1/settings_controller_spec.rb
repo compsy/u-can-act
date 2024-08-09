ddf38a640bf21db5d2b5791ca658a83b7053493a
Ando Emerencia <a.c.emerencia@researchable.nl>
2024-08-09 19:58:45 +0200

    test(rspec): fix specs

--- spec/controllers/api/v1/settings_controller_spec.rb
+++ spec/controllers/api/v1/settings_controller_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe Api::V1::SettingsController, type: :controller do
+describe Api::V1::SettingsController, type: :controller, focus: true do
   describe 'index' do
     def flat_hash(hash, final = [], res = {})
       return res.update(final => hash) unless hash.is_a? Hash
@@ -11,9 +11,10 @@ describe Api::V1::SettingsController, type: :controller do
       res
     end
 
-    let(:settings) { YAML.load_file(Rails.root.join('config/settings.yml')) }
+    let(:settings) { YAML.load_file(Rails.root.join('config/settings.yml'), aliases: true) }
     let(:specific_settings) do
-      YAML.load_file(Rails.root.join('projects', ENV.fetch('PROJECT_NAME', nil), 'config', 'settings.yml'))
+      YAML.load_file(Rails.root.join('projects', ENV.fetch('PROJECT_NAME', nil), 'config', 'settings.yml'),
+                     aliases: true)
     end
 
     let(:current_settings) { settings[Rails.env].deep_merge(specific_settings[Rails.env]) }

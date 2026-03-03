3dde0500bfd7f57e1139a3b4ec432d830cd1bfd1
Anna Gumenyuk <a.gumenyuk@researchable.nl>
2026-03-03 16:26:19 +0100

    feat: time stuff

--- app/generators/time_generator.rb
+++ app/generators/time_generator.rb
@@ -32,7 +32,8 @@ class TimeGenerator < QuestionTypeGenerator
   # rubocop:disable Metrics/ParameterLists
   def time_dropdown(question_id, from_time, to_time, step, label, raw_label, am_pm)
     elem_id = idify(question_id, raw_label)
-    options = generate_dropdown((from_time...to_time).step(BigDecimal(step.to_s)), elem_id, am_pm)
+    invert = question_id == :v2
+    options = generate_dropdown((from_time...to_time).step(BigDecimal(step.to_s)), elem_id, am_pm, invert)
     options = safe_join([
                           options,
                           tag.label(label)
@@ -42,8 +43,8 @@ class TimeGenerator < QuestionTypeGenerator
   # rubocop:enable Metrics/ParameterLists
 
   # rubocop:disable Metrics/PerceivedComplexity
-  def generate_dropdown(items, id, am_pm)
-    items = items.sort_by { |h| h < 12 ? h + 24 : h } if am_pm
+  def generate_dropdown(items, id, am_pm, invert)
+    items = items.sort_by { |h| h < 12 ? h + 24 : h } if am_pm && invert
     body = []
     items.each do |option|
       option_value = number_to_string(option)

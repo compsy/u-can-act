1f6816a8c7b5e869f1409ab784a8cd42425501c5
Ando Emerencia <a.c.emerencia@researchable.nl>
2026-03-16 16:48:25 +0100

    fix: use response.parsed_body per Rails/ResponseParsedBody cop

--- spec/controllers/questionnaire_controller_spec.rb
+++ spec/controllers/questionnaire_controller_spec.rb
@@ -649,9 +649,8 @@ RSpec.describe QuestionnaireController, type: :controller do
 
       it 'returns some error message' do
         post :interactive_render, params: { content: content }
-        parsed = JSON.parse(response.body)
-        expect(parsed).to have_key('error')
-        expect(parsed['error']).to match(/unexpected token/)
+        expect(response.parsed_body).to have_key('error')
+        expect(response.parsed_body['error']).to match(/unexpected token/)
       end
     end
   end

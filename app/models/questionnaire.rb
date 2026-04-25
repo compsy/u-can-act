6c9fce4154b41a10fd83fd75b158269eaf51851d
Mario Rios <mrm994@gmail.com>
2026-04-25 22:06:29 +0200

    fix: missing coder argument for serializer

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -18,7 +18,7 @@ class Questionnaire < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :content, presence: true
   validates :key, presence: true, uniqueness: true, format: { with: /\A[a-z_0-9]+\Z/ }
-  serialize :content # Don't specify Hash type because otherwise databases with existing questionnaires won't work
+  serialize :content, coder: YAML # Don't specify Hash type because otherwise databases with existing questionnaires won't work
   validate :questionnaire_structure, if: -> { content.present? }
   validate :valid_content_translations, if: -> { content.present? }
   with_options if: :content_has_questions do

9f7b975da0b56ce6ad23d67de318ab2683f03e24
Mario Rios <mrm994@gmail.com>
2026-05-04 18:47:23 +0200

    fix: production build error due to unloaded gem

--- config/initializers/json_schema.rb
+++ config/initializers/json_schema.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
+# json-schema is a transient dependency coming from rswag. Rswag is only
+# available on the development and test environments. If we run this initializer
+# on production we get a boot fail. We could just skip it on production, but
+# actually checking for the presence of the gem is more correct.
+return unless Gem.loaded_specs.key?('json-schema')
+
+require 'json-schema'
+
 # multi_json has been deprecated
 JSON::Validator.use_multi_json = false
